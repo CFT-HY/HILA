ca0380bbe2b87f59f7d47c64656053bb0e2daa03
Kari Rummukainen <Kari.Rummukainen@helsinki.fi>
2019-08-07 22:34:26 +0300

    some tests

---
+++ test/test_3.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+#include "field.h"
+
+template <template <typename> class D, typename T=double>
+struct F {
+  D<T> d;
+};
+
+template <typename T=double> 
+struct vector {
+  T data[10];
+};
+
+template <class T, class A>
+class c {
+
+  void sub2(field<T> &a, const field<double> &b, parity p)
+  {
+    // transformer_control("dump-ast");
+    a[p] -= b[X];
+  }
+};
+
+
+//#include "sub.h"
+
+int main() 
+{
+  field<vector<>> a,b;
+  c<double,double> t;
+  F<vector> vd;
+  F<vector,float> vf;
+
+    
+  
+  // t.sub2(a,b,EVEN);
+  
+  return 0;
+}
+
---
+++ test/test_4.cpp
@@ -0,0 +1,39 @@
+
+#include "field.h"
+
+template <int n, typename T>
+struct s {
+  T c[n];
+};
+
+using vec = s<3,double>;
+
+
+template <>
+class field<int>  {
+  int j;
+};
+
+
+// extern field<int> glob;
+
+int main()
+{
+  field<double> lf;
+  field<double> dd;
+  field<double> t;
+  field<int> k;
+  field<vec> sa;
+  // struct s<int> is;
+  
+  lf[ALL] = dd[X]+t[X];
+
+  // is.c = 2;
+
+  
+  // i[ALL] = i[X]+j[X];
+  
+  
+  return 0;
+}
+
