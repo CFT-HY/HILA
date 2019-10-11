4cbb4aaf948c7f6f5a3c8391cd67a80e018bfe4d
Mielonen Eelis V <eelismie@lx7-500-035.astro.helsinki.fi>
2019-10-11 13:41:37 +0300

    first test case added
    Former-commit-id: 98badf11fc57ae746718fd44bc12055dd6549f77

---
+++ programs/test_cases/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+tests=( "$@" )
+
+for test in "${tests[@]}"; do
+    ./$test
+    if [ $? -eq 0 ];
+    then 
+        echo -e "$test \e[36msuccess"
+    else
+        echo -e "$test \e[30mfailed"
+    fi
+done
