c7059b9f4e44532803320f02389130e7254187f5
Jarno Rantaharju <jarno.rantaharju@helsinki.fi>
2019-10-16 17:53:06 +0300

    Run tests in three steps
    Former-commit-id: be2a299496ad222a23c3939e95b906fe41fc79b0

--- programs/test_cases/test.sh
+++ programs/test_cases/test.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+check(){
+    if [ $? -eq 0 ];
+    then
+        echo -e "$1 $test \e[36msuccess\e[39m"
+    else
+        echo -e "$1 $test \e[30mfailede\e[39m"
+    fi
+}
+
+transform_c(){
+    echo ../../build/transformer $@ 2>/dev/null
+    ../../build/transformer $@ 2>/dev/null
+    check transform
+}
+
+compile_c(){
+    echo make $@
+    make $@
+    check compile
+}
+
 tests=( "$@" )
 
 for test in "${tests[@]}"; do
+    transform_c ${test}.cpp
+    compile_c ${test}.cpt
+    echo ./$test
     ./$test
-    if [ $? -eq 0 ];
-    then
-        echo -e "$test \e[36msuccess"
-    else
-        echo -e "$test \e[30mfailed"
-    fi
+    check
+    make clean
 done
+
