258decf4f0a8dd0ce1bdc491f48bfa92af2e89dc
Jarno Rantaharju <jarno.rantaharju@helsinki.fi>
2019-10-18 15:47:48 +0300

    Use the cub library reduction
    Former-commit-id: 82c44b1587935a9ca6f7f2612f2fb17d13100cd1

--- programs/plumbing/hila_cuda.h
+++ programs/plumbing/hila_cuda.h
@@ -8,8 +8,9 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <curand_kernel.h>
+#include <cub/cub.cuh>
 
-#define N_threads 128
+#define N_threads 64
 
 #define loop_callable __host__ __device__
 
@@ -34,23 +35,81 @@ static inline void check_cuda_error(std::string message){
 }
 
 /* Reduction */
-template<typename T>
-__global__ void cuda_reduce_sum_kernel( T * vector, int N)
-{
-  for( int i=1; i<N; i++ ) {
-    vector[0] += vector[i];
-  }
-}
 
 template<typename T>
 T cuda_reduce_sum(  T * vector, int N ){
+  static bool initialized = false;
+  static T * d_sum;
+  static void *d_temp_storage;
+  static size_t temp_storage_size;
+
   T sum;
-  cuda_reduce_sum_kernel<<< 1, 1 >>>( vector, N );
-  check_cuda_error("cuda_reduce_sum kernel");
-  cudaMemcpy( &sum, vector, sizeof(T), cudaMemcpyDeviceToHost );
-  check_cuda_error("Memcpy in reduction");
+
+  if( !initialized ) {
+    cudaMalloc( (void **)&d_sum, sizeof(T) );
+    d_temp_storage = NULL;
+    temp_storage_size = 0;
+    cub::DeviceReduce::Sum(d_temp_storage, temp_storage_size, vector, d_sum, N);
+    initialized = true;
+  }
+  
+  // Allocate temporary storage
+  cudaMalloc(&d_temp_storage, temp_storage_size);
+
+  // Run sum-reduction
+  cub::DeviceReduce::Sum(d_temp_storage, temp_storage_size, vector, d_sum, N);
+
+  cudaMemcpy( &sum, d_sum, sizeof(T), cudaMemcpyDeviceToHost );
+
   return sum;
 }
+
+
+// A simple hand-written reduction that does not require a library
+//
+//template<typename T>
+//__global__ void cuda_reduce_sum_kernel( T * vector, int vector_size, int new_size, int elems)
+//{
+//  int Index = threadIdx.x + blockIdx.x * blockDim.x;
+//  if( Index < new_size ){
+//    for(int i=1; i<elems; i++){
+//      int ind = Index + i*new_size;
+//      if( ind < vector_size ){
+//        vector[Index] += vector[ ind ];
+//      }
+//    }
+//  }
+//}
+//
+//
+//template<typename T>
+//T cuda_reduce_sum(  T * vector, int N ){
+//  const int reduce_step = 32;
+//  T sum;
+//  T * host_vector = (T *)malloc(N*sizeof(T));
+//  int vector_size = N;
+//
+//  while( vector_size > reduce_step ){
+//    int new_size = vector_size/reduce_step + 1;
+//    int blocks = new_size/N_threads + 1;
+//    cuda_reduce_sum_kernel<<< blocks, N_threads >>>( vector, vector_size, new_size, reduce_step );
+//    vector_size = new_size;
+//    cudaDeviceSynchronize();
+//  }
+//  
+//
+//  check_cuda_error("cuda_reduce_sum kernel");
+//  cudaMemcpy( host_vector, vector, vector_size*sizeof(T), cudaMemcpyDeviceToHost );
+//  check_cuda_error("Memcpy in reduction");
+//
+//  for(int i=0; i<vector_size; i++){
+//    sum += host_vector[i];
+//  }
+//
+//  free(host_vector);
+//  
+//  return sum;
+//}
 
 #else
 
