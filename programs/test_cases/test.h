7c4a08737dfecbe900dcb80b5f2d6322ab988372
Jarno Rantaharju <jarno.rantaharju@helsinki.fi>
2019-10-25 15:45:50 +0300

    Add tests for field methods
    Former-commit-id: 77d956b3a3f814cc770af248fa392761f3d2afcc

--- programs/test_cases/test.h
+++ programs/test_cases/test.h
@@ -3,9 +3,11 @@
 #include <math.h>
 #include <assert.h>
 
-#include "../plumbing/field.h"
-#include "../datatypes/general_matrix.h"
+
+#include "../plumbing/defs.h"
 #include "../datatypes/cmplx.h"
+#include "../datatypes/general_matrix.h"
+#include "../plumbing/field.h"
 
 // Direct output to stdout
 std::ostream &hila::output = std::cout;
