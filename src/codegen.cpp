3b3bed57a8619daec8d42963ba2257174e269c2d
Jarno Rantaharju <jarno.rantaharju@helsinki.fi>
2019-11-11 16:32:51 +0200

    Should assign local sum, not add
    Former-commit-id: b1641ec2af0e84043530c0dde9e551e8c901482d

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -163,10 +163,10 @@ void MyASTVisitor::generate_code(Stmt *S, codetype & target) {
     if(target.CUDA){
       // Run reduction
       if (v.reduction_type == reduction::SUM) {
-        code << v.type << " " << v.new_name << "_global += cuda_reduce_sum(" 
+        code << v.type << " " << v.new_name << "_global = cuda_reduce_sum(" 
              << v.new_name << ", lattice->volume()" <<  ");\n";
       } else if (v.reduction_type == reduction::PRODUCT) {
-        code << v.type << " " << v.new_name << "_global *= cuda_reduce_product(" 
+        code << v.type << " " << v.new_name << "_global = cuda_reduce_product(" 
              << v.new_name << ", lattice->volume()" <<  ");\n";
       }
       // Free memory allocated for the reduction
