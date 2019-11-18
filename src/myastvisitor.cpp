37cf88b515cfdb58cacc6e5d8d7712564cb363e3
Kari Rummukainen <Kari.Rummukainen@helsinki.fi>
2019-11-18 22:48:45 +0200

    Split loop function handling to separate file -- easier to contain future logic
    Former-commit-id: dbed434bafd80fccdacebbb09c4ba50456462541

--- src/myastvisitor.cpp
+++ src/myastvisitor.cpp
@@ -303,80 +303,6 @@ void MyASTVisitor::handle_var_ref(DeclRefExpr *DRE,
   }
 }
 
-// Go through each parameter of function calls and handle
-// any field references.
-// Assume non-const references can be assigned to.
-void MyASTVisitor::handle_function_call_in_loop(Stmt * s) {
-  int i=0;
-
-  // Get the call expression
-  CallExpr *Call = dyn_cast<CallExpr>(s);
-
-  // Handle special loop functions
-  if( handle_special_loop_function(Call) ){
-    return;
-  }
-
-  // Get the declaration of the function
-  Decl* decl = Call->getCalleeDecl();
-  FunctionDecl* D = (FunctionDecl*) llvm::dyn_cast<FunctionDecl>(decl);
-
-  // Store functions used in loops, recursively...
-  loop_function_check(decl);
-  for( Expr * E : Call->arguments() ){
-    if( is_field_parity_expr(E) ) {
-      if(i < D->getNumParams()){
-        const ParmVarDecl * pv = D->getParamDecl(i);
-        QualType q = pv->getOriginalType ();
-
-        // Check for const qualifier
-        if( q.isConstQualified ()) {
-          //llvm::errs() << "  -Const \n";
-        } else {
-          handle_field_parity_expr(E, true, false);
-        }
-      }
-    }
-    i++;
-  }
-}
-
-void MyASTVisitor::handle_loop_function(FunctionDecl *fd) {
-  // we should mark the function, but it is not necessarily in the
-  // main file buffer
-
-  if (target.flag_loop_function) {
-  
-    SourceManager &SM = TheRewriter.getSourceMgr();
-    SourceLocation sl = fd->getSourceRange().getBegin();
-    FileID FID = SM.getFileID(sl);
-    set_fid_modified(FID);
-
-    srcBuf * sb = get_file_buffer(TheRewriter, FID);
-    if (target.CUDA) {
-      sb->insert(sl, "__device__ __host__ ",true,true);
-    } else if (target.openacc) {
-      sb->insert(sl, "/* some ACC pragma here */");
-    }
-  }
-}
-
-bool MyASTVisitor::handle_special_loop_function(CallExpr *Call) {
-  // If the function is in a list of defined loop functions, add it to a list
-  // Return true if the expression is a special function and
-  std::string name = Call->getDirectCallee()->getNameInfo().getAsString();
-  if( name == "coordinates" ){
-    llvm::errs() << get_stmt_str(Call) << '\n';
-    special_function_call sfc;
-    sfc.fullExpr = Call;
-    sfc.scope = state::scope_level;
-    sfc.replace_expression = "lattice->coordinates";
-    sfc.add_loop_var = true;
-    special_function_call_list.push_back(sfc);
-    return 1;
-  }
-  return 0;
-}
 
 ///////////////////////////////////////////////////////////////////////////////
 /// handle_full_loop_stmt() is the starting point for the analysis of all
@@ -908,58 +834,7 @@ void MyASTVisitor::check_var_info_list() {
   }
 }
 
-bool MyASTVisitor::loop_function_check(Decl *d) {
-  assert(d != nullptr);
-  
-  FunctionDecl *fd = dyn_cast<FunctionDecl>(d);
-  if (fd) {
-    
-    // fd may point to declaration (prototype) without a body.  
-    // Argument of hasBody becomes the pointer to definition if it is in this compilation unit
-    // needs to be const FunctionDecl *
-    const FunctionDecl * cfd;
-    if (fd->hasBody(cfd)) {
-  
-      // take away const
-      fd = const_cast<FunctionDecl *>(cfd);
-      
-      // check if we already have this function
-      for (int i=0; i<loop_functions.size(); i++) if (fd == loop_functions[i]) return true;
-    
-      llvm::errs() << " ++ callgraph for " << fd->getNameAsString() << '\n';
-    
-      loop_functions.push_back(fd);
-      handle_loop_function(fd);
 
-      // And check also functions called by this func
-      CallGraph CG;
-      // addToCallGraph takes Decl *: cast 
-      CG.addToCallGraph( dyn_cast<Decl>(fd) );
-      // CG.dump();
-      int i = 0;
-      for (auto iter = CG.begin(); iter != CG.end(); ++iter, ++i) {
-        // loop through the nodes - iter is of type map<Decl *, CallGraphNode *>
-        // root i==0 is "null function", skip
-        if (i > 0) {
-          Decl * nd = iter->second->getDecl();
-          assert(nd != nullptr);
-          if (nd != fd) {
-            loop_function_check(nd);
-          }
-        }
-        // llvm::errs() << "   ++ loop_function loop " << i << '\n';
-      }
-      return true;
-    } else {
-      // Now function has no body - could be in other compilation unit or in system library.
-      // TODO: should we handle these?
-      // llvm::errs() << "   Function has no body!\n";
-    }
-  } else {
-    // now not a function - should not happen
-  }
-  return false;
-}
 
 /// flag_error = true by default in myastvisitor.h
 SourceRange MyASTVisitor::getRangeWithSemi(Stmt * S, bool flag_error) {
