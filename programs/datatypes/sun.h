9021400f9c5e96d65a1356bc9e00e2f848b8c00d
Mielonen Eelis V <eelis.mielonen@helsinki.fi>
2019-11-22 15:51:42 +0200

    Implementation moved to header to avoid linker issues
    Former-commit-id: eb195771db95b29a03c0665fec17ba90dd9d1595

--- programs/datatypes/sun.h
+++ programs/datatypes/sun.h
@@ -1,5 +1,8 @@
 #include "cmplx.h"
 #include "general_matrix.h"
+#include "../plumbing/defs.h"
+#include "../plumbing/mersenne.h" //has to be included
+#include <cmath>
 
 //////////////////
 ///
@@ -9,6 +12,8 @@
 /// (parametrized by pauli matrices) for efficiency
 /// otherwise behaves like a regular complex matrix
 ///
+/// Implementation is found in header file so that linker 
+/// finds specializations
 //////////////////
 
 //TODO: SU(2) vector class + conversion to and from matrix rep.
@@ -23,10 +28,10 @@ template<typename radix>
 class SU<2, radix> { 
     public: 
 
-        SU();
-        ~SU();
+        SU(){};
+        ~SU(){};
         SU<2,radix> & normalize(); //normalize elements
-        SU<2,radix> & reunitarize();
+        SU<2,radix> & reunitarize(); 
         SU<2,radix> & random(); //generate random SU2 element 
         SU<2,radix> & inv(); //invert matrix and return reference to self
         radix sqr(); //calculate square of all elements
@@ -51,3 +56,55 @@ class SU<2, radix> {
 
         radix a, b, c, d;      
 };
+
+#define VARIANCE 0.5
+#define MYSINF(X) sin(X)
+#define MYCOSF(X) cos(X)
+
+template<typename radix>
+inline radix gaussian_ran2 (radix* out2) 
+{
+  double phi, urnd, r;
+  phi = 2.0 * 3.141592654 * (double) hila_random();
+  urnd = (double)(1.0-hila_random());
+  r  = sqrt( -log(urnd) * (2.0 * VARIANCE) );
+  *out2 = (r*MYCOSF(phi));
+  return (radix)(r*MYSINF(phi));
+}
+
+template<typename radix>
+radix SU<2,radix>::sqr(){
+    return a*a + b*b + c*c + d*d;
+}
+
+template<typename radix>
+SU<2,radix> & SU<2,radix>::normalize(){
+    radix sq = sqrt(this->sqr());
+    a /= sq;
+    b /= sq;
+    c /= sq;
+    d /= sq;   
+    return *this;
+}
+
+template<typename radix>
+SU<2,radix> & SU<2,radix>::reunitarize(){
+    return this->normalize();
+}
+
+template<typename radix>
+SU<2,radix> & SU<2,radix>::random(){
+    radix one, two;
+    one = gaussian_ran2(&two);
+    a = one;
+    b = two;
+    one = gaussian_ran2(&two);
+    c = one;
+    d = two;
+    return this->normalize();
+}
+
+template<typename radix>
+SU<2,radix> & SU<2,radix>::inv(){
+    return *this;
+}
