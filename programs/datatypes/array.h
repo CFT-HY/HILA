7c460680a3a1c808979d41deab9ede0079abfa7e
Mielonen Eelis V <eelis.mielonen@helsinki.fi>
2019-12-16 13:33:32 +0200

    basic template array class finished
    Former-commit-id: 6b29a5eb8836b4d92bfdd43cee4c44042d0e3ec8

--- programs/datatypes/array.h
+++ programs/datatypes/array.h
@@ -5,23 +5,28 @@ template<typename T, int n>
 class array {
     public:
         T c[n];
-        array(){};
+        array() = default;
         void as_matrix();
+
+        array & operator = (const array<T,n> & rhs){
+            for (int i = 0; i < n; i++) r.c[i] = rhs.c[i];
+        }
 
         friend array<T, n> operator + (const array<T, n> & x, const array<T, n> & y){
             array<T, n> r;
-            for (int i = 0; i < n; i++) r[i] =  typeConj(x[i])*y[i];
+            for (int i = 0; i < n; i++) r.c[i] =  x.c[i] + y.c[i];
             return r;
         }
 
         friend array<T, n> operator - (const array<T, n> & x, const array<T, n> & y){
             array<T, n> r;
+            for (int i = 0; i < n; i++) r.c[i] =  x.c[i] + y.c[i];
             return r;
         }
 
         friend array<T, n> operator * (const array<T, n> & x, const array<T, n> & y){
             array<T, n> r;
-            for (int i = 0; i < n; i++) r[i] =  typeConj(x[i])*y[i];
+            for (int i = 0; i < n; i++) r.c[i] =  typeConj(x.c[i])*y.c[i];
             return r;
         }
 
