3c94de1f9f5806ecb51dcbc2542f292a023090e5
Mielonen Eelis V <eelis.mielonen@helsinki.fi>
2020-01-07 15:28:47 +0200

    changed description on operations.h
    Former-commit-id: 9333e13ca90ae7440648e7ba6b27377fcb71deb8

--- programs/datatypes/operations.h
+++ programs/datatypes/operations.h
@@ -3,22 +3,24 @@
 #define OPS_H
 
 ///////////////////////////////////////////
-// These routines define the default
-// behaviour of trans, conj, etc.
-// routines. These routines alter
-// how operations are performed between
-// members of the same dtype. If they are
-// not specialized for a dtype they do not do anything.
-// Specializations of these templates are
-// found in the headers defining each dtype.
+// The routines are used as operation modifiers
+// i.e. they change how +,-,* are performed
+// between members of the same data type.   
+// By default they do not change any behaviour 
+// unless they are specialized for a dtype. 
+// For example A*trans(B) multiplies A by the
+// transpose of the elements of B when they are 
+// matrices. 
 ////////////////////////////////////////////
 
+//complex conjugate
 template<typename T>
 #pragma transformer loop_function
 inline T conj(T rhs){
   return rhs;
 }
 
+//transpose 
 template<typename T>
 #pragma transformer loop_function
 inline T trans(T rhs){
