a8751686b40a7628bc33cf8dca2464c6236cb28b
Mielonen Eelis V <eelis.mielonen@helsinki.fi>
2020-01-07 13:04:40 +0200

    created new header for special operations on dtypes. This should be added to all headers that use conj(), trans() etc
    Former-commit-id: 4f1e4b4d5cf605d156991b57cebe11c61ef34c41

---
+++ programs/datatypes/operations.h
@@ -0,0 +1,28 @@
+///////////////////////////////////////////
+// These routines define the default
+// behaviour of trans, conj, etc.
+// routines. These routines alter
+// how operations are performed between
+// members of the same dtype. If they are
+// not specialized for a dtype they do not do anything.
+// Specializations of these templates are
+// found in the headers defining each dtype.
+////////////////////////////////////////////
+
+template<typename T>
+#pragma transformer loop_function
+inline T conj(T rhs){
+  return rhs;
+}
+
+template<typename T>
+#pragma transformer loop_function
+inline T trans(T rhs){
+  return rhs;
+}
+
+template<typename T>
+#pragma transformer loop_function
+inline double norm_sq(T val){
+  return val*val;
+}
