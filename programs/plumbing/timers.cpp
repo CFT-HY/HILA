899d3229cf228c926ff54564ee70be8ffe8cc2bd
Kari Rummukainen <Kari.Rummukainen@helsinki.fi>
2020-03-24 21:38:55 +0200

    Ups - a bug still remained in timers
    Former-commit-id: 714f61c639cb38f985f945c15653aad245f3875a

--- programs/plumbing/timers.cpp
+++ programs/plumbing/timers.cpp
@@ -33,13 +33,13 @@ void timer::report(const char * label, int print_header) {
 
     if (print_header > 0 || (first && print_header == -1)) {
       first = false;
-      hila::output << "TIMER:                 total(sec)          calls   usec/call  fraction\n";
+      hila::output << "TIMER REPORT:         total(sec)          calls   usec/call  fraction\n";
     }
     // time used during the counter activity
     t_initial = gettime() - t_initial;
     if (count > 0) {
       std::snprintf(line,200," %16s: %14.3f %14llu %11.3f %8.4f\n",
-                    label, t->total, t->count, 1e6 * t->total/t->count, t->total/t->initial );
+                    label, t_total, count, 1e6 * t_total/count, t_total/t_initial );
     } else {
       std::snprintf(line,200," %16s: no timed calls made\n",label);
     }
