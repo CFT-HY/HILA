8305b5c2ac3323bada61840a9f63bccf0bbe53c3
Kari Rummukainen <Kari.Rummukainen@helsinki.fi>
2020-03-29 14:23:25 +0300

    Make memalloc a function, also somewhat improved error message
    Former-commit-id: d4f6bf3398ca5dd6908282f62332805e32ee99f8

--- programs/plumbing/memalloc.h
+++ programs/plumbing/memalloc.h
@@ -1,23 +1,11 @@
 /// Memory allocator -- gives back aligned memory, if ALIGN defined
-/// This is a hacky method to insert automatic check to success of
-/// memalloc, so that we get the file name and line number where the
-/// the failure happened:
-/// we use just macro with comma operators and assertion instead of separate function
-/// points where the allocation failed.
 
-static void * _memalloc__ptr_;   // this one pollutes namespace, but need to define one
+#include "../plumbing/defs.h"
 
-#ifndef ALIGN
-// _ptr_ is here a tmp variable
-#define memalloc(size) ( _memalloc__ptr_ = std::malloc( size),                    \
-    assert( _memalloc__ptr_ != nullptr && "memalloc: memory allocation failure"), \
-    _memalloc__ptr_ )
+/// We'll have two prototypes, 2nd gives the file name and size for error messages
+/// preprocessor substitutes memalloc() -calls with right parameters!
 
-#else
-#define memalloc(size) ( void * _ptr_,                            \
-    int e = std::posix_memalign( &_ptr_, (std::size_t)32, size),  \
-    assert( e == 0 && "memalloc: memory allocation failure"),     \
-    _ptr_ )
+void * memalloc( std::size_t size);
+void * memalloc( std::size_t size, const char * filename, const unsigned line);
 
-#endif
 
