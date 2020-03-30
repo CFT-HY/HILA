a79cb5bf747a8812a81308957082ce69ca0ac3db
Jarno Rantaharju <jarno.rantaharju@helsinki.fi>
2020-03-30 10:50:20 +0300

    Replace SU vector with a trivial class (necessary for using in a field)
    Former-commit-id: e2127edff6608019b7a37b2b86884840cd02e2a2

--- programs/datatypes/sun_vector.h
+++ programs/datatypes/sun_vector.h
@@ -1,10 +1,46 @@
 #ifndef SU_VEC
 #define SU_VEC
 
-#include "cmplx.h"
-#include "array.h"
+#include "../datatypes/cmplx.h"
+#include "../datatypes/general_matrix.h"
 
-template<int N, typename radix> 
-class SU_vector : public array<N,cmplx<radix>> {};
+
+static double gaussian_ran() 
+{
+  static double VARIANCE = 0.5;
+  static double second;
+  static bool draw_new = true;
+  if( draw_new ) {
+    double first, phi, urnd, r;
+    phi = 2.0 * 3.141592654 * (double) hila_random();
+    urnd = (double)(1.0-hila_random());
+    r  = sqrt( -log(urnd) * (2.0 * VARIANCE) );
+    first = (double)(r*sin(phi));
+    second = (r*cos(phi));
+
+    draw_new = false;
+    return first;
+  } else {
+    draw_new = true;
+    return second;
+  }
+}
+
+
+
+
+template<int n, typename radix>
+class SU_vector : public matrix<n,1,cmplx<radix> >{
+  public:
+  using base_type = typename base_type_struct<radix>::type;
+  
+  void gaussian(){ 
+    for (int i = 0; i < n; i++) {
+      (*this).c[i][0].re = gaussian_ran();
+      (*this).c[i][0].im = gaussian_ran();
+    }
+  }
+
+};
 
 #endif
