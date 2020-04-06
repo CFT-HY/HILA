38a3d8c7ff44f1b3f3a6c1dfa8ef97c593970d96
Eelis Mielonen <eelis.mielonen@helsinki.fi>
2020-04-06 22:29:24 +0300

    vacuum state initialization
    Former-commit-id: 01cd2b637a549165d1c8edc47483df734d3fc8ba

--- programs/scaling/onsim.cpp
+++ programs/scaling/onsim.cpp
@@ -1,6 +1,7 @@
 
 #define _USE_MATH_DEFINES
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <math.h>
 #include <assert.h>
@@ -19,29 +20,34 @@ std::ostream &hila::output = std::cout;
 lattice_struct my_lattice;
 lattice_struct * lattice = & my_lattice;
 
+inline double scalefactor(double t, double t_end){
+	return t/t_end;
+}
+
 int main(int argc, char ** argv){
 
-	input parameters = input(); 
-	parameters.add_essential("l"); //define parameters that must be in parameter file
-	parameters.add_essential("sigma");
-	parameters.add_essential("seed");
-	parameters.add_essential("param1");
-	parameters.import("sim_params.txt"); //import sim_params
-  
-	int l = parameters.get("l");
-	int seed = parameters.get("seed");
-	
-	double param1 = parameters.get("param1");
-	double sigma = parameters.get("sigma");
+	int l = 10;
+	int seed = 20;
+	int steps = 30;
+	double param1 = 1.0;
+	double sigma = 0.1;
 
-	int box_dimensions[3] = {l,l,l}; 
+  	int dim = 3;
+  	dx = 1.0;
+  	dt = 0.1;
+  	tStart = 0.0;
+  	tEnd = 32.0;
+ 
+	int box_dimensions[3] = {l,2*l,4*l}; 
 
 	lattice->setup(box_dimensions, argc, argv);
 	seed_random(seed);
 
 	//define fields
+
 	field<cmplx<double>> phi;
 	field<cmplx<double>> pi;
+	field<cmplx<double>> e; //energy 
 
 	//initialize vaccuum state phi, set pi to zero
  
@@ -52,6 +58,24 @@ int main(int argc, char ** argv){
 		cmplx<double> val;
 		phi[X] = val.polar(r, theta);
 		pi[X] = 0; 
+	}
+
+	//evolve fields
+
+	double t = 0;
+	for (int i = 0; i < steps; i++){
+
+		double a = scaleFactor(t);  
+      		double aHalfPlus = scaleFactor(t+dt/2.0);
+      		double aHalfMinus = scaleFactor(t-dt/2.0);
+
+		double aadt_aadxdx = pow( a / aHalfPlus , Real(2) ) * dt / (dx*dx);
+  		double aadt2D_aadxdx = aadt_aadxdx * Real(2) * dim;
+ 		double aaaaldt_aa = pow( a, Real(4) ) * lambda * dt / pow(aHalfPlus, 2);
+  		double daa_aa = ( pow(aHalfPlus, 2) - pow(aHalfMinus, 2) ) / pow(aHalfPlus, 2);
+  		double ss = sigma*sigma;
+		double*  deltaPi= new double[nf]; //delta-pi at each site
+    		double mod; //at each site 
 	}
 
 	return 0;
