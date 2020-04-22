af4727eae9e355bf0a8ba015373e94f060558754
Mielonen Eelis V <eelis.mielonen@helsinki.fi>
2020-04-22 17:44:06 +0300

    fix issues that lead to simulation diverging under most initial conditions
    Former-commit-id: 2a5d44f7654318eb6ae809e832d780a2403ed4e5

--- programs/scaling/onsim.cpp
+++ programs/scaling/onsim.cpp
@@ -29,6 +29,7 @@ class scaling_sim {
 		scaling_sim() = default;
 		void allocate(const std::string fname, int argc, char ** argv);
 		void initialize();
+		void write_moduli();
 		void write_energies();
 		void next();
 		inline double scaleFactor(double t);
@@ -43,13 +44,16 @@ class scaling_sim {
 			int l;
 			int seed;
 			int smoothing;
-			double param1;
+			double initialModulus;
 			double sigma;
 			double dx;
 			double dt;
 			double tStart;
+			double tStats;
+			double nOutputs;
 			double tEnd;
 			double lambda;
+			std::string ofname;
 		} config;
 };
 
@@ -59,15 +63,18 @@ void scaling_sim::allocate(const std::string fname, int argc, char ** argv){
 	parameters.import(fname);
 	config.l = parameters.get("N");
 	config.seed = parameters.get("seed");
-	config.param1 = parameters.get("param1");
+	config.initialModulus = parameters.get("initialModulus");
 	config.sigma = parameters.get("sigma");
   	config.dx = parameters.get("dx");
-	double ratio = parameters.get("dtdxRatio");
-  	config.dt = config.dx*ratio;
   	config.tStart = parameters.get("tStart");
   	config.tEnd = parameters.get("tEnd");
 	config.lambda = parameters.get("lambda");
 	config.smoothing = parameters.get("smooth");
+	config.tStats = parameters.get("tStats");
+	config.nOutputs = parameters.get("numberStatsOutputs");
+	double ratio = parameters.get("dtdxRatio");
+  	config.dt = config.dx*ratio;
+	t = config.tStart;
 
 	int box_dimensions[3] = {config.l,config.l,config.l}; 
 
@@ -84,7 +91,7 @@ void scaling_sim::initialize(){
 	//initialize vaccuum state
 	onsites(ALL){
 		double theta, r;
-		r = config.param1*config.sigma;
+		r = config.initialModulus*config.sigma;
 		theta = hila_random()*2*M_PI;
 		cmplx<double> val;
 		phi[X] = val.polar(r, theta);
@@ -106,7 +113,28 @@ void scaling_sim::initialize(){
 		}
 	}
 
-	
+}
+
+void scaling_sim::write_moduli(){
+	double phimod = 0.0;
+	double pimod = 0.0;
+
+	onsites(ALL){
+		double p_r = 0.0, p_i = 0.0;
+		cmplx<double> norm_1 = phi[X].conj()*phi[X];
+		cmplx<double> norm_2 = pi[X].conj()*pi[X]; 
+		p_r = norm_1.re;
+		p_i = norm_2.re;
+		phimod += sqrt(p_r);
+		pimod += sqrt(p_i);
+	}
+
+	if (mynode() == 0){
+		double vol = (double) (config.l*config.l*config.l);
+		hila::output << phimod/vol << "," << pimod/vol << ",";
+	}
+
+	synchronize();
 
 }
 
@@ -115,23 +143,24 @@ void scaling_sim::write_energies(){
 	double ss = config.sigma*config.sigma;
 
 	//non-weighted energies
-    	double sumPi = 0.0;
+	double sumPhi = 0.0;
+    double sumPi = 0.0;
    	double sumDiPhi = 0.0;
-    	double sumV = 0.0;
+    double sumV = 0.0;
 	double sumPhiDiPhi = 0.0;
 	double sumPhiPi = 0.0; 
 
 	//weighted energies
-        double w_sumPi = 0.0;
-        double w_sumDiPhi = 0.0;
-        double w_sumV = 0.0;
-        double w_sumPhiDiPhi = 0.0; 
-        double w_sumPhiPi = 0.0;
+    double w_sumPi = 0.0;
+    double w_sumDiPhi = 0.0;
+    double w_sumV = 0.0;
+    double w_sumPhiDiPhi = 0.0; 
+    double w_sumPhiPi = 0.0;
 
 	onsites(ALL){
 		double v = 0;
-                cmplx<double> norm = phi[X].conj()*phi[X]; //calculate phi norm
-                sumV += 0.25*config.lambda*a*a*pow((norm - ss).re, 2.0); //reduce potential
+        cmplx<double> norm = phi[X].conj()*phi[X]; //calculate phi norm
+        sumV += 0.25*config.lambda*a*a*pow((norm.re - ss), 2.0); //reduce potential
 		sumPi += (pi[X].conj()*pi[X]).re; 
 	}
 
@@ -146,7 +175,7 @@ void scaling_sim::write_energies(){
 
 	if (mynode() == 0){
 		double vol = (double) config.l*config.l*config.l;
-		hila::output << sumV/vol << '\t' << sumDiPhi/vol << '\t' << sumPi/vol << '\n';
+		hila::output << a << "," << sumPi/vol << "," << sumDiPhi/vol << "," << sumV/vol << '\n';
 	}
 }
 
@@ -154,28 +183,33 @@ void scaling_sim::next(){
 	double a = scaleFactor(t);
 	double aHalfPlus = scaleFactor(t + config.dt/2.0); 
 	double aHalfMinus = scaleFactor(t - config.dt/2.0);
+
 	double aadt_aadxdx = pow( a / aHalfPlus , 2.0) * config.dt / (config.dx*config.dx);
-  	double aadt2D_aadxdx = aadt_aadxdx * 2.0 * NDIM;
+  	double aadt2D_aadxdx = aadt_aadxdx * 2.0 * 3.0;
  	double aaaaldt_aa = pow( a, 4.0 ) * config.lambda * config.dt / pow(aHalfPlus, 2.0);
   	double daa_aa = ( pow(aHalfPlus, 2.0) - pow(aHalfMinus, 2.0) ) / pow(aHalfPlus, 2.0);
   	double ss = config.sigma*config.sigma;
 
+	deltaPi[ALL] = cmplx<double>(0.0, 0.0);
 	phi[ALL] = phi[X] + config.dt*pi[X];
 
 	onsites(ALL){
 		cmplx<double> norm = phi[X].conj()*phi[X]; //calculate phi norm
-		deltaPi[X] = -1.0*(aadt2D_aadxdx + aaaaldt_aa*(norm - ss))*phi[X]; 	
+		deltaPi[X] = phi[X]*(aaaaldt_aa*(ss - norm.re) - aadt2D_aadxdx); 	
 	}
 
 	direction d;
-	foralldir(d){
-		deltaPi[ALL] += aadt_aadxdx*phi[X + d]; 
+	forALLdir(d){
+		onsites(ALL){
+			deltaPi[X] = deltaPi[X] + aadt_aadxdx*phi[X + d];
+		}
 	}
 
 	pi[ALL] = pi[X] - daa_aa*pi[X];
 	pi[ALL] = pi[X] + deltaPi[X]; 
 
 	t += config.dt;
+	synchronize();
 }
 
 int main(int argc, char ** argv){
@@ -183,8 +217,11 @@ int main(int argc, char ** argv){
 	sim.allocate("sim_params.txt", argc, argv);
 	sim.initialize();
 	while(sim.t < sim.config.tEnd){
-		sim.next();
+		sim.write_moduli();
 		sim.write_energies();
+		sim.next();
 	}
+
+	finishrun();
 	return 0;
 }
