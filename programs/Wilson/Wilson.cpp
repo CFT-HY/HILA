f33b2df16bfeb12781f93d36867c1fcfea6f137b
Jarno Rantaharju <jarno.rantaharju@helsinki.fi>
2020-05-12 11:33:53 +0300

    Simplify a bit
    Former-commit-id: 7869df2cc597af0c449864db62d6b454a1ced42d

--- programs/Wilson/Wilson.cpp
+++ programs/Wilson/Wilson.cpp
@@ -15,19 +15,10 @@ void test_gamma_matrices(){
   w2 = w1-gamma5*(gamma5*w1);
   assert(w2.norm_sq() < 0.0001 && "g5*g5 = 1");
 
-  w2 = w1-gamma0*(gamma0*w1);
-  assert(w2.norm_sq() < 0.0001 && "g0*g0 = 1");
-
-  w2 = w1-gamma1*(gamma1*w1);
-  assert(w2.norm_sq() < 0.0001 && "g1*g1 = 1");
-
-  w2 = w1-gamma2*(gamma2*w1);
-  assert(w2.norm_sq() < 0.0001 && "g2*g2 = 1");
-
-  w2 = w1-gamma3*(gamma3*w1);
-  assert(w2.norm_sq() < 0.0001 && "g3*g3 = 1");
-
   foralldir(d){
+    w2 = w1-gamma_matrix[d]*(gamma_matrix[d]*w1);
+    assert(w2.norm_sq() < 0.0001 && "gamma_d*gamma_d = 1");
+
     w2 = w1 + gamma_matrix[d]*w1;
     h1 = half_Wilson_vector(w1,d);
     double diff = w2.norm_sq() - h1.norm_sq();
