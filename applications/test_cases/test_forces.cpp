e6882ad48a754c05f7d23ccb4597149d9fe20e7d
Jarno Rantaharju <jarno.rantaharju@helsinki.fi>
2020-06-24 13:13:34 +0300

    Fix fore test and remove the separate test forrepresented forces
    Former-commit-id: 0a5430ab9a4d27a5c54f59379b2f1b7a74be04e3

--- applications/test_cases/test_forces.cpp
+++ applications/test_cases/test_forces.cpp
@@ -16,8 +16,7 @@ constexpr int N = 2;
 
 template<typename dirac, typename gauge_field_type>
 void check_forces(dirac &D, gauge_field_type &gauge){
-  using matrix = typename gauge_field_type::gauge_type;
-  using fund_type = typename gauge_field_type::fund_type;
+  using sun = typename gauge_field_type::fund_type;
   using forcetype = squarematrix<gauge_field_type::N, cmplx<double>>;
   field<forcetype> force[NDIM];
   double eps = 1e-5;
@@ -28,10 +27,13 @@ void check_forces(dirac &D, gauge_field_type &gauge){
     gauge.set_unity();
     fa.draw_gaussian_fields();
     gauge.zero_momentum();
+    foralldir(dir){
+      force[dir][ALL] = 0;
+    }
 
-    fund_type g1 = gauge.get_gauge(0).get_value_at(50);
-    fund_type h = fund_type(1) + cmplx(0, eps) * fund_type::generator(ng);
-    fund_type g12 = h*g1;
+    sun g1 = gauge.get_gauge(0).get_value_at(50);
+    sun h = sun(1) + cmplx(0, eps) * sun::generator(ng);
+    sun g12 = h*g1;
 
     field<typename dirac::vector_type> psi, chi, tmp, tmp2;
     #if NDIM > 3
@@ -74,9 +76,9 @@ void check_forces(dirac &D, gauge_field_type &gauge){
     D.force(chi, psi, force, 1);
 
     gauge.add_momentum(force);
-    fund_type f = gauge.get_momentum(0).get_value_at(50);
+    sun f = gauge.get_momentum(0).get_value_at(50);
     double f1 = (s2-s1)/eps;
-    double f2 = (f*cmplx(0,1)*matrix::generator(ng)).trace().re;
+    double f2 = (f*cmplx(0,1)*sun::generator(ng)).trace().re;
     double diff = f2-f1;
 
     if(mynode()==0) {
@@ -124,7 +126,7 @@ void check_forces(dirac &D, gauge_field_type &gauge){
     gauge.add_momentum(force);
     f = gauge.get_momentum(0).get_value_at(50);
     f1 = (s2-s1)/eps;
-    f2 = (f*cmplx(0,1)*matrix::generator(ng)).trace().re;
+    f2 = (f*cmplx(0,1)*sun::generator(ng)).trace().re;
     diff = f2-f1;
 
     if(mynode()==0) {
@@ -154,170 +156,6 @@ void check_forces(dirac &D, gauge_field_type &gauge){
 
     fa.force_step(1.0);
     f = gauge.get_momentum(0).get_value_at(50);
-    f1 = (s2-s1)/eps;
-    f2 = (f*cmplx(0,1)*gauge_field_type::fund_type::generator(ng)).trace().re;
-    diff = f2-f1;
-
-    if(mynode()==0) {
-      //hila::output << "Action 1 " << s1 << "\n";
-      //hila::output << "Action 2 " << s2 << "\n";
-      //hila::output << "Calculated force " << f2 << "\n";
-      //hila::output << "Actual force " << (s2-s1)/eps << "\n";
-      //hila::output << "Fermion force " << ng << " diff " << diff << "\n";
-      assert( diff*diff < eps*10 && "Fermion force" );
-    }
-  }
-}
-
-
-
-
-template<typename dirac, typename sun, typename repr>
-void check_represented_forces(double mass_parameter){
-  gauge_field<SU<N,double>> gauge;
-  represented_gauge_field<repr> rgauge(gauge);
-  field<squarematrix<repr::size,cmplx<double>>> rmom[NDIM];
-  double eps = 1e-5;
-
-  dirac D(mass_parameter, rgauge);
-  fermion_action fa(D, rgauge);
-
-  for(int ng = 0; ng < 1 ; ng++ ){ //sun::generator_count(); ng++){
-    gauge.set_unity();
-    rgauge.refresh();
-    fa.draw_gaussian_fields();
-    foralldir(dir){
-      gauge.momentum[dir][ALL] = 0;
-      rmom[dir][ALL] = 0;
-    }
-
-    sun g1 = gauge.gauge[0].get_value_at(50);
-    sun h = sun(1) + eps * cmplx(0,1)*sun::generator(ng);
-    sun g12 = h*g1;
-
-    field<typename dirac::vector_type> psi, chi, tmp, tmp2;
-    #if NDIM > 3
-    psi.set_boundary_condition(TUP, boundary_condition_t::ANTIPERIODIC);
-    psi.set_boundary_condition(TDOWN, boundary_condition_t::ANTIPERIODIC);
-    #endif
-    chi.copy_boundary_condition(psi);
-    tmp.copy_boundary_condition(psi);
-    tmp2.copy_boundary_condition(psi);
-
-    onsites(D.par){
-      if(disable_avx[X]==0){};
-      psi[X].gaussian();
-      chi[X].gaussian();
-    }
-    
-    double s1 = 0;
-    D.apply(psi,tmp);
-    onsites(D.par){
-      s1 += chi[X].rdot(tmp[X]);
-    }
-
-    if(mynode()==0){
-      gauge.gauge[0].set_value_at(g12,50);
-    }
-    gauge.gauge[0].mark_changed(ALL);
-    rgauge.refresh();
-
-    double s2 = 0;
-    D.apply(psi,tmp);
-    onsites(D.par){
-      s2 += chi[X].rdot(tmp[X]);
-    }
-
-    if(mynode()==0)
-      gauge.gauge[0].set_value_at(g1, 50);
-    gauge.gauge[0].mark_changed(ALL);
-    rgauge.refresh();
-
-
-    D.force(chi, psi, rmom, 1);
-
-    SU<repr::size,double> rf = rmom[0].get_value_at(50);
-    sun f = repr::project_force(rf);
-    double f1 = (s2-s1)/eps;
-    double f2 = (f*cmplx(0,1)*sun::generator(ng)).trace().re;
-    double diff = f2-f1;
-
-    if(mynode()==0) {
-      //hila::output << "Action 1 " << s1 << "\n";
-      //hila::output << "Action 2 " << s2 << "\n";
-      //hila::output << "Calculated deriv " << f2 << "\n";
-      //hila::output << "Actual deriv " << (s2-s1)/eps << "\n";
-      //hila::output << "Fermion deriv " << ng << " diff " << diff << "\n";
-      assert( diff*diff < eps*10 && "Fermion deriv" );
-    }
-
-
-
-    onsites(D.par){
-      if(disable_avx[X]==0){};
-      psi[X].gaussian();
-      chi[X].gaussian();
-    }
-
-    s1 = 0;
-    D.dagger(psi,tmp);
-    onsites(D.par){
-      s1 += chi[X].rdot(tmp[X]);
-    }
-
-    if(mynode()==0){
-      gauge.gauge[0].set_value_at(g12,50);
-    }
-    gauge.gauge[0].mark_changed(ALL);
-    rgauge.refresh();
-
-    s2 = 0;
-    D.dagger(psi,tmp);
-    onsites(D.par){
-      s2 += chi[X].rdot(tmp[X]);
-    }
-
-    if(mynode()==0)
-      gauge.gauge[0].set_value_at(g1, 50);
-    gauge.gauge[0].mark_changed(ALL);
-    rgauge.refresh();
-
-
-    D.force(chi, psi, rmom, -1);
-    rf = rmom[0].get_value_at(50);
-    f = repr::project_force(rf);
-    f1 = (s2-s1)/eps;
-    f2 = (f*cmplx(0,1)*sun::generator(ng)).trace().re;
-    diff = f2-f1;
-
-    if(mynode()==0) {
-      //hila::output << "Action 1 " << s1 << "\n";
-      //hila::output << "Action 2 " << s2 << "\n";
-      //hila::output << "Calculated deriv " << f2 << "\n";
-      //hila::output << "Actual deriv " << (s2-s1)/eps << "\n";
-      //hila::output << "Fermion dg deriv " << ng << " diff " << diff << "\n";
-      assert( diff*diff < eps*10 && "Fermion dg deriv" );
-    }
-
-
-
-    foralldir(dir){
-      gauge.gauge[dir][ALL] = 0;
-    }
-
-    if(mynode()==0){
-      gauge.gauge[0].set_value_at(g12,50);
-    }
-    gauge.gauge[0].mark_changed(ALL);
-    s2 = fa.action();
-
-    if(mynode()==0)
-      gauge.gauge[0].set_value_at(g1, 50);
-    gauge.gauge[0].mark_changed(ALL);
-    s1 = fa.action();
-
-    fa.force_step(1.0);
-    f = gauge.gauge[0].get_value_at(50);
     f1 = (s2-s1)/eps;
     f2 = (f*cmplx(0,1)*sun::generator(ng)).trace().re;
     diff = f2-f1;
@@ -408,6 +246,7 @@ int main(int argc, char **argv){
 
 
 
+  // Staggered Forces
   output0 << "Checking staggered forces:\n";
   dirac_staggered<VEC, SUN> D_Stg(1.5, gauge);
   check_forces<dirac_staggered<VEC, SUN>, gauge_field<SU<N,double>>>(D_Stg, gauge);
@@ -418,28 +257,44 @@ int main(int argc, char **argv){
 
   output0 << "Checking adjoint staggered forces:\n";
   represented_gauge_field<adj> adj_gauge(gauge);
-  dirac_staggered_evenodd<adjvec, adj> D_adj(1.5, adj_gauge);
-  check_forces<dirac_staggered_evenodd<adjvec, adj>,represented_gauge_field<adj>>(D_adj, adj_gauge);
+  dirac_staggered_evenodd<adjvec, adj> D_stg_adj(1.5, adj_gauge);
+  check_forces<dirac_staggered_evenodd<adjvec, adj>,represented_gauge_field<adj>>(D_stg_adj, adj_gauge);
 
   output0 << "Checking symmetric staggered forces:\n";
-  check_represented_forces<dirac_staggered_evenodd<symvec, sym>, SUN, sym>(1.5);
-  
-  output0 << "Checking antisymmetric staggered forces:\n";
-  check_represented_forces<dirac_staggered_evenodd<asymvec, asym>, SUN, asym>(1.5);
-  
-  //output0 << "Checking Wilson forces:\n";
-  //check_forces<Dirac_Wilson<N, double, SUN>, SUN>(0.05);
-  //output0 << "Checking evenodd preconditioned Wilson forces:\n";
-  //check_forces<Dirac_Wilson_evenodd<N, double, SUN>, SUN>(0.05);
-  output0 << "Checking adjoint Wilson forces:\n";
-  check_represented_forces<Dirac_Wilson_evenodd<adj::size, double, adj>, SUN, adj>(0.05);
-  output0 << "Checking symmetric Wilson forces:\n";
-  check_represented_forces<Dirac_Wilson_evenodd<sym::size, double, sym>, SUN, sym>(0.05);
-  output0 << "Checking antisymmetric Wilson forces:\n";
-  check_represented_forces<Dirac_Wilson_evenodd<asym::size, double, asym>, SUN, asym>(0.05);
+  represented_gauge_field<sym> sym_gauge(gauge);
+  dirac_staggered_evenodd<symvec, sym> D_stg_sym(1.5, sym_gauge);
+  check_forces<dirac_staggered_evenodd<symvec, sym>,represented_gauge_field<sym>>(D_stg_sym, sym_gauge);
 
+  output0 << "Checking antisymmetric staggered forces:\n";
+  represented_gauge_field<asym> asym_gauge(gauge);
+  dirac_staggered_evenodd<asymvec, asym> D_stg_asym(1.5, asym_gauge);
+  check_forces<dirac_staggered_evenodd<asymvec, asym>,represented_gauge_field<asym>>(D_stg_asym, asym_gauge);
+  
+
+  // Wilson forces
+  output0 << "Checking evenodd preconditioned Wilson forces:\n";
+  Dirac_Wilson<N, double, SUN> D_W(0.05, gauge);
+  check_forces<Dirac_Wilson<N, double, SUN>, gauge_field<SU<N,double>>>(D_W, gauge);
+
+  output0 << "Checking evenodd preconditioned Wilson forces:\n";
+  Dirac_Wilson_evenodd<N, double, SUN> D_W_eo(0.05, gauge);
+  check_forces<Dirac_Wilson_evenodd<N, double, SUN>, gauge_field<SU<N,double>>>(D_W_eo, gauge);
+
+  output0 << "Checking adjoint Wilson forces:\n";
+  Dirac_Wilson_evenodd<adj::size, double, adj> D_W_adj(1.5, adj_gauge);
+  check_forces<Dirac_Wilson_evenodd<adj::size, double, adj>,represented_gauge_field<adj>>(D_W_adj, adj_gauge);
+
+  output0 << "Checking symmetric Wilson forces:\n";
+  Dirac_Wilson_evenodd<sym::size, double, sym> D_W_sym(1.5, sym_gauge);
+  check_forces<Dirac_Wilson_evenodd<sym::size, double, sym>,represented_gauge_field<sym>>(D_W_sym, sym_gauge);
+
+  output0 << "Checking antisymmetric Wilson forces:\n";
+  Dirac_Wilson_evenodd<asym::size, double, asym> D_W_asym(1.5, asym_gauge);
+  check_forces<Dirac_Wilson_evenodd<asym::size, double, asym>,represented_gauge_field<asym>>(D_W_asym, asym_gauge);
+
+
+  // Check also the momentum action and derivative
   for(int ng = 0; ng < SU<N>::generator_count(); ng++){
-    // Check also the momentum action and derivative
     ga.draw_gaussian_fields();
 
     double s1 = ga.action();
