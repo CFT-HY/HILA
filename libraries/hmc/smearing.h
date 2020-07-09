2b0c946ee22548ddc31b0d2c0ebe896052457247
Jarno Rantaharju <jarno.rantaharju@helsinki.fi>
2020-07-09 16:28:56 +0300

    Working on fixing the force, correct without the staple
    Former-commit-id: c74740ad87f4554be83bff133288c610a7156671

--- libraries/hmc/smearing.h
+++ libraries/hmc/smearing.h
@@ -15,7 +15,7 @@ struct stout_smeared_field {
   static constexpr int N = sun::size;
 
   double c;
-  int exp_steps = 5;
+  int exp_steps = 10;
 
   gauge_field<sun> &fundamental;
   field<sun> gauge[NDIM];
@@ -37,6 +37,7 @@ struct stout_smeared_field {
       field<sun> staple[NDIM];
       staple[dir] = calc_staples(fundamental.gauge, dir);
       onsites(ALL){
+        staple[dir][X] = 1;
         staple[dir][X] = -c*fundamental.gauge[dir][X]*staple[dir][X];
         project_antihermitean(staple[dir][X]);
         staple[dir][X].exp(exp_steps);
@@ -64,34 +65,38 @@ struct stout_smeared_field {
     foralldir(dir){
       result[dir][ALL] = 0;
       staple[dir] = calc_staples(fundamental.gauge, dir);
-      Qfield[dir] = calc_staples(fundamental.gauge, dir);
       onsites(ALL){
+        staple[dir][X] = 1;
         Qfield[dir][X] = -c*fundamental.gauge[dir][X]*staple[dir][X];
         project_antihermitean(Qfield[dir][X]);
       }
     }
     foralldir(dir){
       onsites(ALL){
-        element<sun> m0, m1, qn, iq, ex1, Q, R, L;
+        element<sun> m0, m1, qn, ex1, Q, R, L;
         Q = Qfield[dir][X];
         R = fundamental.gauge[dir][X];
         L = force[dir][X];
-        Lambda[dir][X] = 0;
-        m1=0; ex1=0; qn=1;
-        iq = cmplx(0,1)*Q;
+        ex1=1; qn=Q;
         double n=1.0;
-        m0 = cmplx(0,1)*R*L;
+        m0 = R*L;
+        m1 = m0;
+        if(exp_steps==0){
+          Lambda[dir][X] = 0;
+        } else {
+          Lambda[dir][X] = m1;
+        }
         // gamma matrix (morningstar paper, eq 74)
-        for(int k=0; k<exp_steps; k++){
-          m1 = m0*qn+iq*m1;
-          ex1 = ex1+n*qn; //ex1 = e^(iQ) 
-          qn = qn*iq;
+        for(int k=2; k<=exp_steps; k++){
+          m1 = m0*qn+Q*m1;
+          ex1 = ex1+n*qn; //ex1 = e^(iQ)
+          qn = qn*Q;
           // lambda = 1/(n+1)!*m1
           n=n*1.0/((double)(k+1));
           //output0 << " * " << 1.0/n << "\n":
-          Lambda[dir][X] = c*Lambda[dir][X] + m1*n;
-          project_antihermitean(Lambda[dir][X]);
+          Lambda[dir][X] = Lambda[dir][X] + m1*n;
         }
+        project_antihermitean(Lambda[dir][X]);
         result[dir][X] = R*L*ex1;
       }
 
@@ -99,10 +104,11 @@ struct stout_smeared_field {
         element<sun> lambda, stp, R, tmp;
         stp = staple[dir][X];
         R = fundamental.gauge[dir][X];
-        lambda = Lambda[dir][X];
+        lambda = R*force[dir][X];
 
-        result[dir][X] -= R*stp*lambda;
-        Lambda[dir][X] = lambda*R;
+        project_antihermitean(lambda);
+        result[dir][X] -= c*R*stp*lambda;
+        Lambda[dir][X] = 0*lambda*R;
       }
     }
     foralldir(dir1) foralldir(dir2){
@@ -113,8 +119,8 @@ struct stout_smeared_field {
         U2 = fundamental.gauge[dir2][X+dir1];
         U3 = fundamental.gauge[dir1][X+dir2];
         U4 = fundamental.gauge[dir2][X];
-        L = Lambda[dir1][X];
-        L2 = Lambda[dir1][X+dir2];
+        L = 0*Lambda[dir1][X];
+        L2 = 0*Lambda[dir1][X+dir2];
 
         stapleder2[X] = U2*(L*U4*U3).conjugate();
         stapleder3[X] = U3*U2.conjugate()*L*U4;
