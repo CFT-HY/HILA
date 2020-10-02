6121325dbae82f1457730c432d3f8e6e136a54ee
Jarno Rantaharju <jarno.rantaharju@helsinki.fi>
2020-10-02 11:09:59 +0300

    Clean up a bit and include MRE in Hasenbusch actions
    Former-commit-id: 3b60d0e22d752168c837555fc0120a454a8e829e

--- libraries/hmc/fermion_field.h
+++ libraries/hmc/fermion_field.h
@@ -2,10 +2,10 @@
 #define FERMION_FIELD_H
 
 #include "gauge_field.h"
-#include "../../libraries/dirac/Hasenbusch.h"
-#include "../dirac/conjugate_gradient.h"
+#include "dirac/Hasenbusch.h"
+#include "dirac/conjugate_gradient.h"
+#include "MRE_guess.h"
 #include <cmath>
-
 
 
 
@@ -17,7 +17,6 @@ class fermion_action : action_base{
     gauge_field &gauge;
     DIRAC_OP &D;
     field<vector_type> chi;
-    bool use_MRE_guess = true;
 
     // We save a few previous invertions to build an initial guess.
     // old_chi contains a list of these
@@ -99,93 +98,23 @@ class fermion_action : action_base{
     // Build an initial guess for the fermion matrix inversion
     // by inverting first in the limited space of a few previous
     // solutions. These are saved in old_chi.
-    void MRE_guess(field<vector_type> & psi, field<vector_type> & ){
-      double M[MRE_size][MRE_size];
-      double v[MRE_size];
-      field<vector_type> basis[MRE_size];
-      field<vector_type> tmp;
-
-      // Build an orthogonal basis from the previous solutions
-      for(int i=0; i<MRE_size; i++) {
-        // Start with the original solution vector
-        basis[i][ALL] = old_chi_inv[i][X];
-        // Remove the projected components of all previous vectors
-        for(int j=0; j<i; j++){
-          double vdot = 0, norm = 0;
-          onsites(D.par) {
-            norm += basis[i][X].rdot(basis[i][X]);
-            vdot += basis[j][X].rdot(basis[i][X]);
-          }
-          if(norm*norm > 1e-32){
-            basis[i][D.par] -= vdot/norm*basis[j][X];
-          }
-        }
-      }
-
-      // Build the projected matrix, M[i][j] = v[i].v[j]
-      for(int i=0; i<MRE_size; i++) {
-        field<vector_type> Dchi, DDchi;
-        D.apply(basis[i], Dchi);
-        D.dagger(Dchi, DDchi);
-        for(int j=0; j<MRE_size; j++) {
-          double sum = 0;
-          onsites(D.par) {
-            sum += basis[j][X].rdot(DDchi[X]);
-          }
-          M[j][i] = sum;
-        }
-      }
-      // And the projected vector
-      for(int i=0; i<MRE_size; i++){
-        double sum = 0;
-        onsites(D.par){
-          sum += basis[i][X].rdot(chi[X]);
-        }
-        v[i] = sum;
-      }
-
-      // Now invert the small matrix M (Gaussian elimination)
-      for(int i=0; i<MRE_size; i++){
-        // Check that the diagonal element is nonzero
-        if(M[i][i]*M[i][i] > 1e-32){ 
-          // Normalize the i:th row
-          double diag_inv = 1.0/M[i][i];
-          for(int j=0; j<MRE_size; j++) {
-            M[i][j] *= diag_inv;
-          }
-          v[i] *= diag_inv;
-          // Subtract from all other rows
-          for(int k=0; k<MRE_size; k++) if(k!=i){
-            double weight = M[k][i];
-            for(int j=0; j<MRE_size; j++) {
-              M[k][j] -= weight*M[i][j];
-            }
-            v[k] -= weight*v[i];
-          }
-        } else {
-          // In the matrix element is too small, just remove it from the basis
-          v[i] = 0;
-          for(int j=0; j<MRE_size; j++) { 
-            M[j][i] = 0;
-          }
-        }
-      }
-
-      // Construct the solution in the original basis
-      psi[ALL] = 0;
-      for(int i=0; i<MRE_size; i++) if(!isnan(v[i])){
-        psi[D.par] += v[i]*basis[i][X];
+    void initial_guess(field<vector_type> & chi, field<vector_type> & psi){
+      if(MRE_size > 0){
+        MRE_guess(psi, chi, D, old_chi_inv);
+      } else {
+        psi[ALL]=0;
       }
     }
 
     // Add new solution to the list
     void save_new_solution(field<vector_type> & psi){
-      for(int i=1; i<MRE_size; i++){
-        old_chi_inv[i] = old_chi_inv[i-1];
+      if(MRE_size > 0){
+        for(int i=1; i<MRE_size; i++){
+          old_chi_inv[i] = old_chi_inv[i-1];
+        }
+        old_chi_inv[0] = psi;
       }
-      old_chi_inv[0] = psi;
     }
-
 
 
     // Update the momentum with the derivative of the fermion
@@ -195,19 +124,13 @@ class fermion_action : action_base{
       psi.copy_boundary_condition(chi);
       Mpsi.copy_boundary_condition(chi);
       field<momtype> force[NDIM], force2[NDIM];
-      CG<DIRAC_OP> inverse(D);
 
+      CG<DIRAC_OP> inverse(D);
       gauge.refresh();
 
-      if(MRE_size > 0){
-        MRE_guess(psi, chi);
-      } else {
-        psi[ALL]=0;
-      }
+      initial_guess(chi, psi);
       inverse.apply(chi, psi);
-      if(MRE_size > 0){
-        save_new_solution(psi);
-      }
+      save_new_solution(psi);
 
       D.apply(psi, Mpsi);
 
@@ -249,7 +172,10 @@ class Hasenbusch_action_1 : public action_base {
     fermion_action<gauge_field, Hasenbusch_operator<DIRAC_OP>> base_action;
     double _mh;
 
-    Hasenbusch_action_1(DIRAC_OP &d, gauge_field &g, double mh) : _mh(mh), D_h(d, mh), base_action(D_h, g) {}
+    Hasenbusch_action_1(DIRAC_OP &d, gauge_field &g, double mh)
+     : _mh(mh), D_h(d, mh), base_action(D_h, g) {}
+    Hasenbusch_action_1(DIRAC_OP &d, gauge_field &g, double mh, int mre_guess_size)
+     : _mh(mh), D_h(d, mh), base_action(D_h, g, mre_guess_size) {}
 
     Hasenbusch_action_1(Hasenbusch_action_1 &fa) : _mh(fa._mh), D_h(fa.D_h), base_action(fa.base_action){}
 
@@ -272,21 +198,46 @@ class Hasenbusch_action_2 : public action_base {
     Hasenbusch_operator<DIRAC_OP> D_h;
     double mh;
     field<vector_type> chi;
+    
+    // We save a few previous invertions to build an initial guess.
+    // old_chi contains a list of these
+    int MRE_size = 0;
+    std::vector<field<vector_type>> old_chi_inv;
 
-    Hasenbusch_action_2(DIRAC_OP &d, gauge_field &g, double _mh) : mh(_mh), D(d), D_h(d, _mh), gauge(g) {
+    Hasenbusch_action_2(DIRAC_OP &d, gauge_field &g, double _mh) 
+     : mh(_mh), D(d), D_h(d, _mh), gauge(g) {
       chi = 0.0;
       #if NDIM > 3
       chi.set_boundary_condition(TUP, boundary_condition_t::ANTIPERIODIC);
       chi.set_boundary_condition(TDOWN, boundary_condition_t::ANTIPERIODIC);
       #endif
     }
+    Hasenbusch_action_2(DIRAC_OP &d, gauge_field &g, double _mh, int mre_guess_size) 
+     : mh(_mh), D(d), D_h(d, _mh), gauge(g) {
+      chi = 0.0;
+      #if NDIM > 3
+      chi.set_boundary_condition(TUP, boundary_condition_t::ANTIPERIODIC);
+      chi.set_boundary_condition(TDOWN, boundary_condition_t::ANTIPERIODIC);
+      #endif
+      MRE_size = mre_guess_size;
+      old_chi_inv.resize(MRE_size);
+      for(int i=0; i<MRE_size; i++){
+        old_chi_inv[i][ALL] = 0;
+      }
+    }
 
-    Hasenbusch_action_2(Hasenbusch_action_2 &fa) : mh(fa.mh), D(fa.D), D_h(fa.D_h), gauge(fa.gauge){
+    Hasenbusch_action_2(Hasenbusch_action_2 &fa) 
+     : mh(fa.mh), D(fa.D), D_h(fa.D_h), gauge(fa.gauge){
       chi = fa.chi;
       #if NDIM > 3
       chi.set_boundary_condition(TUP, boundary_condition_t::ANTIPERIODIC);
       chi.set_boundary_condition(TDOWN, boundary_condition_t::ANTIPERIODIC);
       #endif
+      MRE_size = fa.MRE_size;
+      old_chi_inv.resize(MRE_size);
+      for(int i=0; i<MRE_size; i++){
+        old_chi_inv[i][ALL] = 0;
+      }
     }
 
 
@@ -337,6 +288,29 @@ class Hasenbusch_action_2 : public action_base {
       D.apply(psi, chi);
     }
 
+
+    // Build an initial guess for the fermion matrix inversion
+    // by inverting first in the limited space of a few previous
+    // solutions. These are saved in old_chi.
+    void initial_guess(field<vector_type> & chi, field<vector_type> & psi){
+      if(MRE_size > 0){
+        MRE_guess(psi, chi, D, old_chi_inv);
+      } else {
+        psi[ALL]=0;
+      }
+    }
+
+    // Add new solution to the list
+    void save_new_solution(field<vector_type> & psi){
+      if(MRE_size > 0){
+        for(int i=1; i<MRE_size; i++){
+          old_chi_inv[i] = old_chi_inv[i-1];
+        }
+        old_chi_inv[0] = psi;
+      }
+    }
+
+
     // Update the momentum with the derivative of the fermion
     // action
     void force_step(double eps){
@@ -352,8 +326,9 @@ class Hasenbusch_action_2 : public action_base {
 
       D_h.dagger(chi, Dhchi); 
 
-      psi[ALL]=0;
-      inverse.apply(Dhchi, psi);
+      initial_guess(chi, psi);
+      inverse.apply(chi, psi);
+      save_new_solution(psi);
 
       D.apply(psi, Mpsi);
 
