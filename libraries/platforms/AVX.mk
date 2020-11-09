30bd2b08feb44b1fe46c2cb9c4f2c9609a9eb497
Kari Rummukainen <rummukai@dx6-flafo-04.theorphys.helsinki.fi>
2020-11-09 12:46:32 +0200

    Add default incl paths for hilapp
    Former-commit-id: 0ffd00894a2bf60195cb8dc1702c36e36eadfdba

--- libraries/platforms/AVX.mk
+++ libraries/platforms/AVX.mk
@@ -15,6 +15,12 @@ LD := mpic++
 CXXFLAGS  := -O3 -x c++ --std=c++17
 #CXXFLAGS := -g -x c++ --std=c++17
 
+
+## The following incantation gives the include paths of the $(CC) compiler (if it is gcc or clang)
+# It may be that this path is not necessary at all, usually not for "system installed" clang
+# THIS SEEMS TO CONFLICT WITH AVX DEFINITIONS; SO LEAVE OUT
+#STD_INCLUDE_DIRS := $(addprefix -I, $(shell echo | $(CC) -xc++ --std=c++17 -Wp,-v - 2>&1 | grep "^ "))
+
 ### Need to give MPI include directory for hilapp - here 2 common ones
 # MPI_INCLUDE_DIRS = -I/usr/lib/x86_64-linux-gnu/openmpi/include -I/usr/lib/openmpi/include
 #
@@ -30,6 +36,6 @@ LDFLAGS :=
 
 # These variables must be defined here
 #
-HILAPP_OPTS := -target:AVX -DAVX -DUSE_MPI $(MPI_INCLUDE_DIRS)
+HILAPP_OPTS := -target:AVX -DAVX -DUSE_MPI $(STD_INCLUDE_DIRS) $(MPI_INCLUDE_DIRS)
 HILA_OPTS := -DAVX -DUSE_MPI
 
--- libraries/platforms/vanilla.mk
+++ libraries/platforms/vanilla.mk
@@ -15,6 +15,12 @@ LD := mpic++
 CXXFLAGS  := -O3 -x c++ --std=c++17
 #CXXFLAGS := -g -x c++ --std=c++17
 
+
+## The following incantation gives the include paths of the $(CC) compiler (if it is gcc or clang)
+# It may be that this path is not necessary at all, usually not for "system installed" clang
+STD_INCLUDE_DIRS := $(addprefix -I, $(shell echo | $(CC) -xc++ --std=c++17 -Wp,-v - 2>&1 | grep "^ "))
+
+
 ### Need to give MPI include directory for hilapp - here 2 common ones
 # MPI_INCLUDE_DIRS = -I/usr/lib/x86_64-linux-gnu/openmpi/include -I/usr/lib/openmpi/include
 #
@@ -29,7 +35,7 @@ LDFLAGS :=
 
 # These variables must be defined here
 #
-HILAPP_OPTS := -DUSE_MPI $(MPI_INCLUDE_DIRS)
+HILAPP_OPTS := $(STD_INCLUDE_DIRS) -DUSE_MPI $(MPI_INCLUDE_DIRS)
 HILA_OPTS := -DUSE_MPI
 
 
