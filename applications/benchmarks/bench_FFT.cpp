567a281c047a9e69ef9ccbb8fc6a78d9cca70962
Jarno Rantaharju <jarno.rantaharju@helsinki.fi>
2020-12-03 14:02:10 +0200

    FFT benchmark
    Former-commit-id: 417368ed5e918ae47fe8c2ac141449f78f704028

---
+++ applications/benchmarks/bench_FFT.cpp
@@ -0,0 +1,68 @@
+#include "bench.h"
+#include "plumbing/timers.h"
+
+#ifndef SEED
+#define SEED 100
+#endif
+
+int main(int argc, char **argv){
+
+  using T = matrix<2,2,cmplx<double>>;
+  using Tf = matrix<2,2,cmplx<float>>;
+
+  bench_setup(argc, argv);
+  seed_random(SEED);
+
+  field<T> d, d2;
+
+  // Generate a random field
+  onsites(ALL){
+    d[X].random();
+  }
+
+  // Run once to make sure everything is set up
+  FFT_field(d, d2);
+  
+  struct timeval start, end;
+  double timing = 0;
+  int n_runs;
+  for(n_runs=1; timing < mintime; ){
+    n_runs*=2;
+    gettimeofday(&start, NULL);
+    for( int i=0; i<n_runs; i++){
+      FFT_field(d, d2);
+    }
+    // // synchronize();
+    gettimeofday(&end, NULL);
+    timing = timediff(start, end);
+    broadcast(timing);
+  }
+  timing = timing / (double)n_runs;
+  output0 << "FFT double precision : "<< timing << " ms \n";
+
+  // Generate a random field
+  field<Tf> f, f2;
+
+  // Generate a random field
+  onsites(ALL){
+    f[X].random();
+  }
+
+  timing = 0;
+  for(n_runs=1; timing < mintime; ){
+    n_runs*=2;
+    gettimeofday(&start, NULL);
+    for( int i=0; i<n_runs; i++){
+      FFT_field(f, f2);
+    }
+    // // synchronize();
+    gettimeofday(&end, NULL);
+    timing = timediff(start, end);
+    broadcast(timing);
+  }
+  timing = timing / (double)n_runs;
+  output0 << "FFT single precision : "<< timing << " ms \n";
+
+
+  finishrun();
+}
