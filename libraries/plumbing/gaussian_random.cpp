9915c61cf1f4fa1d77e0e356a804e4494245be3c
Kari Rummukainen <kari.rummukainen@helsinki.fi>
2020-12-31 16:08:49 +0200

    Add gaussian_random, was missing...
    Former-commit-id: cfe67f6a7ac358610d00b063c4b998675bac53f7

---
+++ libraries/plumbing/gaussian_random.cpp
@@ -0,0 +1,56 @@
+#ifndef RANDOM_H_
+#define RANDOM_H_
+
+#include <cmath>
+#include "plumbing/random.h"
+#include "plumbing/defs.h"
+
+// gaussian rng generation routines ----------------------
+
+
+static constexpr double pi = 3.14159265358979;
+
+#pragma hila loop_function
+double gaussian_ran(double variance=0.5) {
+  static double second;
+  static bool draw_new = true;
+  if( draw_new ) {
+    double first, phi, urnd, r;
+    phi = 2.0 * pi * hila_random();
+    urnd = (1.0-hila_random());
+    r  = sqrt( -log(urnd) * (2.0 * variance) );
+    first =  r*sin(phi);
+    second =  r*cos(phi);
+
+    draw_new = false;
+    return first;
+  } else {
+    draw_new = true;
+    return second;
+  }
+}
+
+#define VARIANCE 0.5
+
+#pragma hila loop_function
+double gaussian_ran2(double & out2) {
+
+  double phi, urnd, r;
+  phi = 2.0 * pi * hila_random();
+  urnd =  1.0 - hila_random();
+  r  = sqrt( -log(urnd) * (2.0 * VARIANCE) );
+  out2 = r*cos(phi);
+  return r*sin(phi);
+}
+
+#pragma hila loop_function
+float gaussian_ran2(float & out2) {
+  double phi, urnd, r;
+  phi = 2.0 * pi * hila_random();
+  urnd =  1.0 - hila_random();
+  r  = sqrt( -log(urnd) * (2.0 * VARIANCE) );
+  out2 = (float) (r*cos(phi));
+  return (float) (r*sin(phi));
+}
+
+#endif
