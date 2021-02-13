aaeea28339bf60653c3d75e63d8332eca35d36e7
Kari Rummukainen <Kari.Rummukainen@helsinki.fi>
2021-02-13 16:56:58 +0200

    Add general purpose header "hila.h", for applications
    Former-commit-id: 08cb881d45e93644de293ccb0bb3a6448b6a5097

---
+++ libraries/plumbing/hila.h
@@ -0,0 +1,17 @@
+#ifndef HILA_H_
+#define HILA_H_
+
+///////////////////////////////////////////////////////////////////////
+/// Catch-(almost)all include to get in most of the hila-system .h -files
+
+#include "plumbing/defs.h"
+#include "plumbing/coordinates.h"
+#include "plumbing/lattice.h"
+#include "plumbing/field.h"
+#include "datatypes/cmplx.h"
+#include "datatypes/matrix.h"
+#include "plumbing/param_input.h"
+
+
+
+#endif
