3e469eb53f3dec926004f023d6c7ce1edc7c6f33
Kari Rummukainen <kari.rummukainen@helsinki.fi>
2021-04-14 11:04:13 +0300

    change std:: to ::std::  in namespace hila in type_tools.h, puhti-cuda complains otherwise (must be bug in compiler). Also add path discovery to puhti_cuda
    Former-commit-id: 9ea93b87bec461e7fcc54a9f324567b6caeba620

--- libraries/target_arch/puhti_cuda.mk
+++ libraries/target_arch/puhti_cuda.mk
@@ -7,6 +7,12 @@
 # module load gcc/9.1.0 cuda/11.1.0 openmpi/4.0.5-cuda
 #
 
+$(info ########################################################################)
+$(info Target puhti_cuda:  remember to )
+$(info   module load gcc/9.1.0 cuda/11.1.0 openmpi/4.0.5-cuda )
+$(info ########################################################################)
+
+
 # Define compiler
 CC = nvcc
 LD = nvcc -gencode arch=compute_70,code=sm_70
@@ -15,6 +21,8 @@ LD = nvcc -gencode arch=compute_70,code=sm_70
 CXXFLAGS = -dc -O3 -x cu -gencode arch=compute_70,code=sm_70 -DCUDA -DUSE_MPI  $(CXX_INCLUDE)
 
 #CXXFLAGS = -g -x c++ --std=c++17 
+
+STD_INCLUDE_DIRS := $(addprefix -I, $(shell echo | g++ -xc++ --std=c++17 -Wp,-v - 2>&1 | grep "^ "))
 
 # No need to give include directory to mpi for hilapp - here 2 common ones
 MPI_INCLUDE_DIRS = 
@@ -30,7 +38,7 @@ HILA_OBJECTS += build/hila_cuda.o
 
 # These variables must be defined here
 #
-HILAPP_OPTS = -target:CUDA -DCUDA -DUSE_MPI -DPUHTI $(MPI_INCLUDE_DIRS)
+HILAPP_OPTS = -target:CUDA -DCUDA -DUSE_MPI -DPUHTI $(STD_INCLUDE_DIRS) $(MPI_INCLUDE_DIRS)
 HILA_OPTS = -DUSE_MPI -DCUDA -DPUHTI
 
 
