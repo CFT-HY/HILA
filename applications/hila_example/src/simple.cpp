5a49120b6006456bfa9a183b7fb03014f142dbb0
Kari Rummukainen <kari.rummukainen@helsinki.fi>
2021-11-21 18:46:30 +0200

    Formatting change for simple.cpp
    Former-commit-id: 4b33cff9a4dd3a156ee2078098dc46e21220d5c8

--- applications/hila_example/src/simple.cpp
+++ applications/hila_example/src/simple.cpp
@@ -2,13 +2,13 @@
 
 static_assert(NDIM == 3, "NDIM must be 3 here");
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
 
     // initialize system
-    hila::initialize(argc,argv);
+    hila::initialize(argc, argv);
 
     // set up 32^3 lattice
-    lattice->setup({32,32,32});
+    lattice->setup({32, 32, 32});
 
     // Random numbers are used here
     hila::seed_random(32345);
@@ -18,11 +18,11 @@ int main(int argc, char * argv[]) {
     Field<double> g = 0;
 
     // make f Gaussian random distributed
-    onsites(ALL) f[X].gaussian();
+    onsites(ALL) f[X].gaussian_random();
 
     // calculate sum of 2nd derivatives of f to g
     foralldir(d) {
-        g[ALL] += abs(f[X+d] - 2*f[X] + f[X-d]);
+        g[ALL] += abs(f[X + d] - 2 * f[X] + f[X - d]);
     }
 
     // get average value of g
@@ -31,8 +31,8 @@ int main(int argc, char * argv[]) {
         ave += g[X];
     }
 
-    output0 << "Average of g is " << ave/lattice->volume() << '\n';
+    output0 << "Average of g is " << ave / lattice->volume() << '\n';
 
     // make a clean exit
-    hila::finishrun();    
+    hila::finishrun();
 }
