a4224978ea736df5480f7b3a0f9cfbe7008a1add
Kari Rummukainen <Kari.Rummukainen@helsinki.fi>
2021-11-24 17:02:02 +0200

    Add new header file where some trivial functions with real arguments are collected
    Former-commit-id: 43e0e7e11f55d8574e398efe378dca9d845ff4b6

---
+++ libraries/plumbing/real_var_ops.h
@@ -0,0 +1,50 @@
+#ifndef REAL_VAR_OPS_H_
+#define REAL_VAR_OPS_H_
+
+#include "plumbing/defs.h"
+#include "plumbing/random.h"
+
+
+
+//////////////////////////////////////////////////////////////////////////
+
+// define real(), imag(), conj() -functions for basic arithmetic types
+template <typename T, std::enable_if_t<hila::is_arithmetic<T>::value, int> = 0>
+inline T real(T val) {
+    return val;
+}
+
+template <typename T, std::enable_if_t<hila::is_arithmetic<T>::value, int> = 0>
+inline T imag(T val) {
+    return (T)0;
+}
+
+template <typename T, std::enable_if_t<hila::is_arithmetic<T>::value, int> = 0>
+inline T conj(T val) {
+    return val;
+}
+
+// define squarenorm and norm
+template <typename T, std::enable_if_t<hila::is_arithmetic<T>::value, int> = 0>
+inline T squarenorm(T val) {
+    return val * val;
+}
+
+template <typename T, std::enable_if_t<hila::is_arithmetic<T>::value, int> = 0>
+inline T norm(T val) {
+    return sqrt(val * val);
+}
+
+// this defines gaussian_random(T & arg) for all double -> T convertible types
+template <typename T, std::enable_if_t<hila::is_arithmetic<T>::value, int> = 0>
+inline void gaussian_random(T &d) {
+    d = static_cast<T>(hila::gaussrand());
+}
+
+// this does the same for std. random
+template <typename T, std::enable_if_t<hila::is_arithmetic<T>::value, int> = 0>
+inline void random(T &d) {
+    d = static_cast<T>(hila::random());
+}
+
+#endif
