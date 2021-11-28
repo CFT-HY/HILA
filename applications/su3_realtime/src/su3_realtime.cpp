560cd75ba05492ed46a6412b402b042db4f08837
Kari Rummukainen <Kari.Rummukainen@helsinki.fi>
2021-11-28 22:10:10 +0200

    Added functionality to su3_realtime
    Former-commit-id: 358f6ba0d75b7918bd9926769059eee46b1e2139

--- applications/su3_realtime/src/su3_realtime.cpp
+++ applications/su3_realtime/src/su3_realtime.cpp
@@ -11,10 +11,10 @@ void update_E(const GaugeField<group> &U, VectorField<Algebra<group>> &E,
 
     Field<group> staple;
 
-    foralldir(d) {
+    foralldir (d) {
         staplesum(U, staple, d);
 
-        onsites(ALL) {
+        onsites (ALL) {
             E[d][X] -= delta * (U[d][X] * staple[X].dagger()).project_to_algebra();
         }
     }
@@ -24,40 +24,67 @@ template <typename group>
 void update_U(GaugeField<group> &U, const VectorField<Algebra<group>> &E,
               double delta) {
 
-    foralldir(d) {
-        onsites(ALL) U[d][X] = exp(E[d][X] * delta) * U[d][X];
+    foralldir (d) {
+        onsites (ALL)
+            U[d][X] = exp(E[d][X] * delta) * U[d][X];
     }
 }
 
 template <typename group>
 void regroup_gauge(GaugeField<group> &U) {
-    foralldir(d) {
-        onsites(ALL) U[d][X].reunitarize();
+    foralldir (d) {
+        onsites (ALL)
+            U[d][X].reunitarize();
     }
 }
 
 template <typename group>
 double measure_plaq(const GaugeField<group> &U) {
+
     Reduction<double> plaq;
-    plaq = 0;
     plaq.allreduce(false).delayed(true);
 
-    foralldir(dir1) foralldir(dir2) if (dir1 < dir2) {
-        onsites(ALL) {
-            plaq += real(trace(U[dir1][X] * U[dir2][X + dir1] *
-                               U[dir1][X + dir2].dagger() * U[dir2][X].dagger()));
-        }
-    }
+
+    foralldir (dir1)
+        foralldir (dir2)
+            if (dir1 < dir2) {
+                onsites (ALL) {
+                    plaq += group::size() - real(trace(U[dir1][X] * U[dir2][X + dir1] *
+                                                       U[dir1][X + dir2].dagger() *
+                                                       U[dir2][X].dagger()));
+                }
+            }
     return plaq.value();
 }
 
 
 static double degauss_quality = 1e-12;
 
+template <typename group>
+void measure_stuff(GaugeField<group> &U, VectorField<Algebra<group>> &E, int trajectory,
+                   int n, double dt) {
+
+    auto plaq = measure_plaq(U);
+
+    double e2;
+    foralldir (d)
+        e2 += E[d].squarenorm();
+    e2 /= 2;
+
+    Field<Algebra<group>> g;
+    get_gauss_violation(U, E, g);
+    auto viol = g.squarenorm();
+
+    output0 << "Measure_start " << n << "\n";
+    output0 << "MEAS " << trajectory << ' ' << n << ' ' << plaq << ' ' << e2 << ' '
+            << viol << '\n';
+    output0 << "Measure_end " << n << "\n";
+}
+
 
 template <typename group>
-void thermalize(GaugeField<group> &U, VectorField<Algebra<group>> &E, int iterations,
-                double dt) {
+void thermalize(GaugeField<group> &U, VectorField<Algebra<group>> &E, double g2Ta,
+                int iterations, double dt) {
 
     regroup_gauge(U);
 
@@ -65,22 +92,15 @@ void thermalize(GaugeField<group> &U, VectorField<Algebra<group>> &E, int iterat
 
     therm_timer.start();
     for (int loop = 0; loop < iterations; loop++) {
-        foralldir(d) onsites(ALL) E[d][X].gaussian_random(0.5);
+        foralldir (d)
+            onsites (ALL)
+                E[d][X].gaussian_random(sqrt(g2Ta));
 
         degauss(U, E, degauss_quality);
 
-            auto pl = measure_plaq(U);
-            double e2 = 0;
-            foralldir(d) {
-                e2 += E[d].squarenorm();
-            }
-
-            output0 << " Before: Plaq 1: " << pl << " E^2 " << e2 << " action " << e2/4 - pl 
-                    << '\n';
-
         // 1/2 timestep for U first
         update_U(U, E, dt / 2);
-        // do 2 time units of evolution with leapfrog
+        // do 1 time unit of evolution with leapfrog
         for (int steps = 0; steps < 1.0 / dt - 1; steps++) {
             update_E(U, E, dt);
             update_U(U, E, dt);
@@ -89,21 +109,41 @@ void thermalize(GaugeField<group> &U, VectorField<Algebra<group>> &E, int iterat
         update_E(U, E, dt);
         update_U(U, E, dt / 2);
 
-         pl = measure_plaq(U);
-        e2 = 0;
-            foralldir(d) {
-                e2 += E[d].squarenorm();
-            }
+        double pl = measure_plaq(U);
+        double e2 = 0;
+        foralldir (d) { e2 += E[d].squarenorm(); }
 
-            output0 << " After: Plaq 1: " << pl << " E^2 " << e2 << " action " << e2/4 - pl 
-                    << '\n';
+        output0 << "THERM: Plaq: " << pl << " E^2 " << e2 << " action "
+                << e2 / 2 + 2 * pl << '\n';
 
         regroup_gauge(U);
-
     }
     therm_timer.stop();
 }
 
+////////////////////////////////////////////////////////////////
+
+template <typename group>
+void do_trajectory(GaugeField<group> &U, VectorField<Algebra<group>> &E, int trajectory,
+                   int trajlen, int measure_interval, double dt) {
+
+
+    for (int n = 0; n < trajlen; n += measure_interval) {
+        update_U(U, E, dt / 2);
+        // do 2 time units of evolution with leapfrog
+        for (int steps = 0; steps < measure_interval - 1; steps++) {
+            update_E(U, E, dt);
+            update_U(U, E, dt);
+        }
+        // and bring U and E to the same time value
+        update_E(U, E, dt);
+        update_U(U, E, dt / 2);
+
+        measure_stuff(U, E, trajectory, n, dt);
+    }
+}
+
+/////////////////////////////////////////////////////////////////
 
 int main(int argc, char **argv) {
 
@@ -126,7 +166,13 @@ int main(int argc, char **argv) {
     CoordinateVector lsize;
     lsize = par.get("lattice size"); // reads NDIM numbers
 
-    int loops = par.get("updates");
+    double g2Ta = par.get("g^2 Ta");
+    double dt = par.get("dt");
+    int trajlen = par.get("trajectory length");
+    int n_traj = par.get("number of trajectories");
+    int measure_interval = par.get("measurement interval");
+    int n_thermal = par.get("thermalisation");
+    int n_thermal_start = par.get("thermalisation start");
     long seed = par.get("random seed");
 
     par.close(); // file is closed also when par goes out of scope
@@ -142,16 +188,18 @@ int main(int argc, char **argv) {
     GaugeField<SU3> U;
     VectorField<Algebra<SU3>> E;
 
-    foralldir(d) {
-        onsites(ALL) U[d][X].gaussian_random(0.3).reunitarize();
+    // some initial noise for gauge field
+    foralldir (d) {
+        onsites (ALL)
+            U[d][X].gaussian_random(0.3).reunitarize();
     }
 
-    thermalize(U, E, 20, 0.02);
+    thermalize(U, E, g2Ta, n_thermal_start, dt);
 
-    double n = 0;
-    foralldir(d) n += E[d].squarenorm();
-
-    output0 << " E square norm " << n / (NDIM * lattice->volume()) << '\n';
+    for (int trajectory = 0; trajectory < n_traj; trajectory++) {
+        thermalize(U, E, g2Ta, n_thermal, dt);
+        do_trajectory(U, E, trajectory, trajlen, measure_interval, dt);
+    }
 
 
     hila::finishrun();
