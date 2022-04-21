d995458d53e4d0699ec30dc85964078a81c00401
Haarti Aaron B <haaaaron@dx6-flafo-05.theorphys.helsinki.fi>
2022-04-21 23:41:03 +0300

    almost works
    Former-commit-id: 1c6dd124352c5080d78ff048c1244b3e18551814

--- applications/hila_example/src/minmax.cpp
+++ applications/hila_example/src/minmax.cpp
@@ -13,23 +13,27 @@ int main(int argc, char *argv[]) {
     hila::initialize(argc, argv);
 
     // set up 32^3 lattice
-    lattice->setup({42, 42, 42});
+    lattice->setup({256, 256, 256});
 
     // Random numbers are used here - use time to seed
     hila::seed_random(0);
 
     // lattice field
-    Field<double> g = 1.0;
+    Field<double> g = 2.0;
 
     // make f Gaussian random distributed
-    g[{0,0,0}] = 2.0;
-    g[{0,7,0}] = 2.5;
+    g[{70,23,0}] = 2.5;
+    g[{94,6,1}] = 1.0;
 
-    double val;
-    CoordinateVector loc;
-    val = g.max(loc);
-    output0 << "Min value " << val << '\n';
-    output0 << "Location:" << loc << '\n';
+    double val1;
+    double val2;
+    CoordinateVector loc1;
+    CoordinateVector loc2;
+    val1 = g.min(loc1);
+    val2 = g.max(loc2);
+    //val = g.max(loc);
+    output0 << "Min value " << val1 << " at location: " << loc1 << '\n';
+    output0 << "Max value " << val2 << " at location: " << loc2 << '\n';
 
     hila::finishrun();
     return 0;
