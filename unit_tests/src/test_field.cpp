b95d2c5ad7173f51f67864d2e7016b01157fc451
aaron <aaron.haarti@protonmail.com>
2022-06-16 23:49:55 +0300

    field test coming along
    Former-commit-id: b6cda4168276360839e8d5543f2aeac181bc2ce3

--- unit_tests/src/test_field.cpp
+++ unit_tests/src/test_field.cpp
@@ -5,22 +5,40 @@ using MyType = double;
 
 struct FieldTest : public testing::Test {
 public:
-    Field<double> dummy_field;
+    Field<float> dummy_field;
+
+    template <typename T>
+    Field<float> temporary_field(T assign_val) {
+        Field<float> temporary_field = assign_val;
+        return temporary_field;
+    }
 };
 
-TEST_F(FieldTest, TestFieldCreationNULL) {
+TEST_F(FieldTest, NullptrConstructor) {
     EXPECT_FALSE(dummy_field.is_allocated());
 }
 
-TEST_F(FieldTest, TestFieldAllocation) {
+TEST_F(FieldTest, Allocation) {
     dummy_field.allocate();
     EXPECT_TRUE(dummy_field.is_allocated());
 }
 
-TEST_F(FieldTest, TestFieldCopy) {
-    dummy_field = 1;
-    auto dummy_field_copy = dummy_field;
+TEST_F(FieldTest, ScalarConstructor) {
     onsites(ALL) {
-        EXPECT_EQ(dummy_field_copy[X],dummy_field[X]);
+        EXPECT_EQ(temporary_field(1)[X],1.0);
     }
 }
+
+TEST_F(FieldTest, ZeroConstructor) {
+    onsites(ALL) {
+        EXPECT_EQ(temporary_field(0)[X],0);
+    }
+}
+
+TEST_F(FieldTest, CopyConstructor) {
+    onsites(ALL) dummy_field[X] = hila::gaussrand();
+    onsites(ALL) {
+        EXPECT_EQ(temporary_field(dummy_field)[X],dummy_field[X]);
+    }
+}
+
