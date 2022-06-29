bb7c4db2a1c204a0c30dd41f04e968e61f9224fe
aaron <aaron.haarti@protonmail.com>
2022-06-29 11:01:16 +0300

    changes
    Former-commit-id: 04e6c6fa1da1a6357f08a776264a9189193d2482

--- unit_tests/src/test_coordinates.cpp
+++ unit_tests/src/test_coordinates.cpp
@@ -3,10 +3,11 @@
 
 using MyType = float;
 
-class TestCoordinateVector {
+struct TestCoordinateVector {
 
 public:
     CoordinateVector dummy_vector = {0,0,0};
+    Vector<3,int> vector = {0,0,0};
 };
 
 TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector constructor from CoordinateVector", "[CoordinateVectorCreation]") {
@@ -14,9 +15,58 @@ TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector constructor from Coordi
     REQUIRE(temporary_vector == dummy_vector);
 }
 
-TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector constructor from vector", "[CoordinateVectorCreation]") {
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector constructor from initializer list", "[CoordinateVectorCreation]") {
     CoordinateVector temporary_vector({0,0,0});
     REQUIRE(temporary_vector == dummy_vector);
 }
 
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector constructor from vector", "[CoordinateVectorCreation]") {
+    CoordinateVector temporary_vector(vector);
+    REQUIRE(temporary_vector == dummy_vector);
+}
+
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector constructor from zero", "[coordinateVectorCreation]") {
+    CoordinateVector temporary_vector(0);
+    REQUIRE(temporary_vector == dummy_vector);
+}
+
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector constructor from direction", "[coordinateVectorCreation]") {
+    CoordinateVector temporary_vector(e_x);
+    dummy_vector = {1,0,0};
+    REQUIRE(temporary_vector == dummy_vector);
+}
+
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector assignment from zero", "[coordinateVectorAssignment]") {
+    CoordinateVector temporary_vector;
+    temporary_vector = 0;
+    REQUIRE(temporary_vector == dummy_vector);
+}
+
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector assignment from coordinateVector", "[coordinateVectorAssignment]") {
+    CoordinateVector temporary_vector;
+    temporary_vector = dummy_vector;
+    REQUIRE(temporary_vector == dummy_vector);
+}
+
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector assignment from initializer list", "[coordinateVectorAssignment]") {
+    CoordinateVector temporary_vector;
+    temporary_vector = {0,0,0};
+    REQUIRE(temporary_vector == dummy_vector);
+}
+
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector indexing with int", "[CoordinateVectorIndexing]") {
+    CoordinateVector temporary_vector({0,1,2});
+    for (int i = 0; i < NDIM; i++) {
+        REQUIRE(temporary_vector[i] == i);
+    }
+}
+
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector indexing with direction", "[CoordinateVectorIndexing]") {
+    CoordinateVector temporary_vector({0,1,2});
+    CHECK(temporary_vector[e_x] == 0);
+    CHECK(temporary_vector[e_y] == 1);
+    REQUIRE(temporary_vector[e_z] == 2);
+}
+
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector parity check")
 
