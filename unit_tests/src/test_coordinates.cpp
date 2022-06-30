093c18e5cfba24b52fd957362c99ab1546501c19
aaron <aaron.haarti@protonmail.com>
2022-06-30 11:12:32 +0300

    coordinatevector tests almost done
    Former-commit-id: 807a3c3946ff23775b4262cd8752afb0d7544f76

--- unit_tests/src/test_coordinates.cpp
+++ unit_tests/src/test_coordinates.cpp
@@ -3,10 +3,12 @@
 
 struct TestCoordinateVector {
     CoordinateVector dummy_vector = {0,0,0};
-    Vector<3,int> vector = {0,0,0};
+    CoordinateVector x = {1,0,0};
+    CoordinateVector y = {0,1,0};
+    CoordinateVector z = {0,0,1};
 };
 
-TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector constructor", "[CoordinateVectorCreation]") {
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector constructor", "[CoordinateVector]") {
     SECTION("Constructor from CoordinateVector") {
         CoordinateVector temporary_vector(dummy_vector);
         REQUIRE(temporary_vector == dummy_vector);
@@ -16,6 +18,7 @@ TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector constructor", "[Coordin
         REQUIRE(temporary_vector == dummy_vector);
     }
     SECTION("Construction from Vector<NDIM,T>") {
+        Vector<3,int> vector = {0,0,0};
         CoordinateVector temporary_vector(vector);
         REQUIRE(temporary_vector == dummy_vector);        
     }
@@ -30,7 +33,7 @@ TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector constructor", "[Coordin
     }
 }
 
-TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector assignment", "[coordinateVectorAssignment]") {
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector assignment", "[coordinateVector]") {
     CoordinateVector temporary_vector;
     SECTION("Assignment from zero") {
         temporary_vector = 0;
@@ -46,7 +49,7 @@ TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector assignment", "[coordina
     }
 }
 
-TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector indexing", "[CoordinateVectorIndexing]") {
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector indexing", "[CoordinateVector]") {
     CoordinateVector temporary_vector({0,1,2});
     SECTION("Indexing with int") {
         for (int i = 0; i < NDIM; i++) {
@@ -60,12 +63,57 @@ TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector indexing", "[Coordinate
     }
 }
 
-TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector parity check", "[CoordinateVectorIndexing]") {
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector parity check", "[CoordinateVector]") {
     CoordinateVector temporary_vector(0);
-    CHECK(temporary_vector.parity() == Parity::even);
+    REQUIRE(temporary_vector.parity() == Parity::even);
     REQUIRE((temporary_vector+=e_y).parity() == Parity::odd);
 }
 
-TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector translation", "[CoordinateVectorTranslation]") {
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector translation", "[CoordinateVector]") {
+    SECTION("Translation with CoordinateVector") {
+        REQUIRE((dummy_vector+=x) == x);
+        REQUIRE((dummy_vector-=x) == dummy_vector);
+    }
+    SECTION("Translation with direction (unit vector)") {
+        REQUIRE((dummy_vector+=e_x)==x);
+        dummy_vector-=e_x;
+        REQUIRE((dummy_vector+=e_y)==y);
+        dummy_vector-=e_y;
+        REQUIRE((dummy_vector+=e_z)==z);
+        dummy_vector-=e_z;
+    }
+    SECTION("Translation with arithmetic") {
+        REQUIRE((dummy_vector+x) ==   x );
+        REQUIRE((dummy_vector-x) == (-x));
+        REQUIRE((dummy_vector+y) ==   y );
+        REQUIRE((dummy_vector-y) == (-y));
+        REQUIRE((dummy_vector+z) ==   z );
+        REQUIRE((dummy_vector-z) == (-z));
+    }
+    SECTION("Translation with direction arithmetic") {
+        REQUIRE((dummy_vector+e_x) ==   x );
+        REQUIRE((dummy_vector-e_x) == (-x));
+        REQUIRE((dummy_vector+e_y) ==   y );
+        REQUIRE((dummy_vector-e_y) == (-y));
+        REQUIRE((dummy_vector+e_z) ==   z );
+        REQUIRE((dummy_vector-e_z) == (-z));
+        REQUIRE((e_x + e_x) == (x + x));
+        REQUIRE((e_y + e_y) == (y + y));
+        REQUIRE((e_z + e_z) == (z + z));
+        REQUIRE((e_x - e_x) == dummy_vector );
+        REQUIRE((e_y - e_y) == dummy_vector );
+        REQUIRE((e_z - e_z) == dummy_vector );
+    }
+    SECTION("Scaling with product") {
+        REQUIRE((2*e_x) == 2*x);
+        REQUIRE((2*e_y) == 2*y);
+        REQUIRE((2*e_z) == 2*z);
+    }
+}
 
+TEST_CASE_METHOD(TestCoordinateVector, "CoordinateVector mathematical methods", "[CoordinateVector]") {
+    SECTION("Coordinate_vector dot product") {
+        REQUIRE((x.dot(x)) == 1);
+        REQUIRE((x.dot(y)) == 0);
+    }
 }
