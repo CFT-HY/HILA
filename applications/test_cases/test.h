0d3b5c804b04ada9a8590eca89a0cf847d5f78e8
Kari Rummukainen <kari.rummukainen@helsinki.fi>
2023-01-17 21:30:48 +0200

    test_cases: make test_FFT to compile

--- applications/test_cases/test.h
+++ applications/test_cases/test.h
@@ -6,9 +6,9 @@
 
 #include "plumbing/defs.h"
 #include "datatypes/cmplx.h"
-#include "datatypes/sun.h"
+// #include "datatypes/sun.h"
 #if NDIM == 4
-#include "datatypes/wilson_vector.h"
+// #include "datatypes/wilson_vector.h"
 #endif
 #include "plumbing/field.h"
 
@@ -18,24 +18,10 @@
 
 const CoordinateVector nd = {32, 32, 32, 32};
 
-inline void checkLatticeSetup() {
-    for (int dir = 0; dir < NDIRS; dir++) {
-        // check that neighbor arrays are allocated
-        assert(lattice.neighb[dir] != nullptr);
-#ifdef CUDA
-        assert(lattice.backend_lattice->d_neighb[dir] != nullptr);
-#endif
-    }
-    for (int dir = 0; dir < NDIM; dir++) {
-        assert(lattice.size(dir) == nd[dir]);
-    }
-}
 
 inline void test_setup(int &argc, char **argv) {
     hila::initialize(argc, argv);
     lattice.setup(nd);
-
-    checkLatticeSetup();
 
     hila::seed_random(1);
 }
