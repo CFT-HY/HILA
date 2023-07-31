c551a11a7ebccfe937d2bb993e1c7acc9332b48e
Haaaaron <aaron.haarti@protonmail.com>
2023-07-31 16:17:51 +0300

    cleaned up suN_gauge to suN_gauge_bare, reorganized file structure a bit

--- applications/suN_gauge/src/suN_gauge_bare.cpp
+++ applications/suN_gauge/src/suN_gauge_bare.cpp
@@ -1,80 +1,15 @@
-#include "hila.h"
-#include "gauge/staples.h"
-#include "gauge/polyakov.h"
-#include "gauge/stout_smear.h"
+#include "suN_gauge_bare.h"
+#include "parameters.h"
+#include "checkpoint.h"
 
-#include "gauge/sun_heatbath.h"
-#include "gauge/sun_overrelax.h"
-
-#include <fftw3.h>
-
-using mygroup = SU<NCOLOR, double>;
-
-enum class poly_limit { OFF, RANGE, PARABOLIC };
-
-
-// define a struct to hold the input parameters: this
-// makes it simpler to pass the values around
-struct parameters {
-    double beta;
-    double deltab;
-    int n_overrelax;
-    int n_update;
-    int n_trajectories;
-    int n_thermal;
-    int n_save;
-    int n_profile;
-    std::string config_file;
-    double time_offset;
-    poly_limit polyakov_pot;
-    double poly_min, poly_max, poly_m2;
-    std::vector<int> n_smear;
-    double smear_coeff;
-    std::vector<int> z_smear;
-    int n_surface;
-    int n_dump_polyakov;
-};
-
-template <typename T>
-void staplesum_db(const GaugeField<T> &U, Field<T> &staples, Direction d1, Parity par,
-                  double deltab) {
-
-    Field<T> lower;
-
-    // zero staples just in case
-    staples[par] = 0;
-
-    foralldir(d2) if (d2 != d1) {
-
-        // anticipate that these are needed
-        // not really necessary, but may be faster
-        U[d2].start_gather(d1, ALL);
-        U[d1].start_gather(d2, par);
-
-        // calculate first lower 'U' of the staple sum
-        // do it on opp parity
-        onsites(opp_parity(par)) {
-            double m;
-            if (2 * X.z() < lattice.size(e_z))
-                m = 1.0 - deltab;
-            else
-                m = 1.0 + deltab;
-
-            lower[X] = m * U[d2][X].dagger() * U[d1][X] * U[d2][X + d1];
-        }
-
-        // calculate then the upper 'n', and add the lower
-        // lower could also be added on a separate loop
-        onsites(par) {
-            double m;
-            if (2 * X.z() < lattice.size(e_z))
-                m = 1.0 - deltab;
-            else
-                m = 1.0 + deltab;
-
-            staples[X] += m * U[d2][X] * U[d1][X + d2] * U[d2][X + d1].dagger() + lower[X - d2];
-        }
-    }
+/**
+ * @brief Helper function to get valid z-coordinate index
+ *
+ * @param z
+ * @return int
+ */
+int z_ind(int z) {
+    return (z + lattice.size(e_z)) % lattice.size(e_z);
 }
 
 template <typename group>
@@ -98,85 +33,6 @@ void measure_stuff(const GaugeField<group> &U, const parameters &p) {
     // write the -(polyakov potential) first, this is used as a weight factor in aa
 
     hila::out0 << plaq << ' ' << poly << '\n';
-}
-
-
-/**
- * @brief Helper function to get valid z-coordinate index
- *
- * @param z
- * @return int
- */
-int z_ind(int z) {
-    return (z + lattice.size(e_z)) % lattice.size(e_z);
-}
-
-
-template <typename group>
-void checkpoint(const GaugeField<group> &U, int iteration, const parameters &p) {
-
-    double t = hila::gettime();
-    // save config
-    U.config_write(p.config_file);
-
-    // write run_status file
-    if (hila::myrank() == 0) {
-        std::ofstream outf;
-        outf.open("run_status", std::ios::out | std::ios::trunc);
-        outf << "iteration   " << iteration + 1 << '\n';
-        outf << "seed        " << static_cast<uint64_t>(hila::random() * (1UL << 61)) << '\n';
-        outf << "time        " << hila::gettime() << '\n';
-        outf.close();
-    }
-
-    std::stringstream msg;
-    msg << "Checkpointing, time " << hila::gettime() - t;
-    hila::timestamp(msg.str().c_str());
-}
-
-
-template <typename group>
-bool restore_checkpoint(GaugeField<group> &U, int &trajectory, parameters &p) {
-
-    uint64_t seed;
-    bool ok = true;
-    p.time_offset = 0;
-
-    hila::input status;
-    if (status.open("run_status", false, false)) {
-
-        hila::out0 << "RESTORING FROM CHECKPOINT:\n";
-
-        trajectory = status.get("iteration");
-        seed = status.get("seed");
-        p.time_offset = status.get("time");
-        status.close();
-
-        hila::seed_random(seed);
-
-        U.config_read(p.config_file);
-
-        ok = true;
-    } else {
-
-        std::ifstream in;
-        in.open(p.config_file, std::ios::in | std::ios::binary);
-
-        if (in.is_open()) {
-            in.close();
-
-            hila::out0 << "READING initial config\n";
-
-            U.config_read(p.config_file);
-
-            ok = true;
-        } else {
-
-            ok = false;
-        }
-    }
-
-    return ok;
 }
 
 template <typename group>
@@ -337,7 +193,7 @@ int main(int argc, char **argv) {
     hila::timer update_timer("Updates");
     hila::timer measure_timer("Measurements");
 
-    restore_checkpoint(U, start_traj, p);
+    // restore_checkpoint(U, start_traj, p);
 
     // We need random number here
     if (!hila::is_rng_seeded())
@@ -370,9 +226,9 @@ int main(int argc, char **argv) {
             measure_timer.stop();
         }
 
-        if (p.n_save > 0 && (trajectory + 1) % p.n_save == 0) {
-            checkpoint(U, trajectory, p);
-        }
+        // if (p.n_save > 0 && (trajectory + 1) % p.n_save == 0) {
+        //     checkpoint(U, trajectory, p);
+        // }
     }
 
     hila::finishrun();
