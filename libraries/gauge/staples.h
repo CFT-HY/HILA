7e573291c59877ba2f7914a4ce64e213953339cf
Haaaaron <aaron.haarti@protonmail.com>
2023-08-03 17:41:07 +0300

    adding suN_gauge_surface

--- libraries/gauge/staples.h
+++ libraries/gauge/staples.h
@@ -1,16 +1,29 @@
+/** @file staples.h */
+
 #ifndef STAPLESUM_H_
 #define STAPLESUM_H_
 
 #include "hila.h"
 
-/////////////////////////////////////////////////////////////////////////////
-/// Sum the staples of link matrices to direction dir
-/// we could do
-/// foralldir(d2) if (d2 != d1)
-///    stapes[par] += U[d2][X]*U[d1][X+d2]*U[d2][X+d1].dagger()  +
-///                   U[d2][X-d2].dagger()*U[d1][X-d2]*U[d2][X-d2+d1]
-/// but let us do it a bit more optimized way
-
+/**
+ * @brief Sum the staples of link matrices to direction dir
+ *
+ * Naive method is to compute:
+ *
+ * \code {.cpp}
+ * foralldir(d2) if (d2 != d1)
+ *     stapes[par] += U[d2][X]*U[d1][X+d2]*U[d2][X+d1].dagger()  +
+ *                    U[d2][X-d2].dagger()*U[d1][X-d2]*U[d2][X-d2+d1]
+ * \endcode
+ *
+ * But the method is computed in a slightly more optimized way
+ *
+ * @tparam T
+ * @param U GaugeField to compute staples for
+ * @param staples Filed to compute staplesum into at each lattice point
+ * @param d1 Direction to compute staplesum for
+ * @param par Parity to compute staplesum for
+ */
 template <typename T>
 void staplesum(const GaugeField<T> &U, Field<T> &staples, Direction d1, Parity par = ALL) {
 
@@ -41,48 +54,6 @@ void staplesum(const GaugeField<T> &U, Field<T> &staples, Direction d1, Parity p
             onsites(par) {
                 staples[X] += U[d2][X] * U[d1][X + d2] * U[d2][X + d1].dagger() + lower[X - d2];
             }
-        }
-    }
-}
-
-template <typename T>
-void staplesum_db(const GaugeField<T> &U, Field<T> &staples, Direction d1, Parity par,
-                  double deltab) {
-
-    Field<T> lower;
-
-    // zero staples just in case
-    staples[par] = 0;
-
-    foralldir(d2) if (d2 != d1) {
-
-        // anticipate that these are needed
-        // not really necessary, but may be faster
-        U[d2].start_gather(d1, ALL);
-        U[d1].start_gather(d2, par);
-
-        // calculate first lower 'U' of the staple sum
-        // do it on opp parity
-        onsites(opp_parity(par)) {
-            double m;
-            if (2 * X.z() < lattice.size(e_z))
-                m = 1.0 - deltab;
-            else
-                m = 1.0 + deltab;
-
-            lower[X] = m * U[d2][X].dagger() * U[d1][X] * U[d2][X + d1];
-        }
-
-        // calculate then the upper 'n', and add the lower
-        // lower could also be added on a separate loop
-        onsites(par) {
-            double m;
-            if (2 * X.z() < lattice.size(e_z))
-                m = 1.0 - deltab;
-            else
-                m = 1.0 + deltab;
-
-            staples[X] += m * U[d2][X] * U[d1][X + d2] * U[d2][X + d1].dagger() + lower[X - d2];
         }
     }
 }
