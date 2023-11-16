52fde63034c457837a5558671a5f2b39a58eecc1
Jaakko Hällfors <local_jaakhall@dx2-528-26839.ad.helsinki.fi>
2023-11-16 12:58:12 +0200

    cmdline finished.

--- libraries/plumbing/cmdline.cpp
+++ libraries/plumbing/cmdline.cpp
@@ -7,20 +7,51 @@
 #include <limits.h>
 #include "cmdline.h"
 #include "hila.h"
-
 using strvec = std::vector<std::string>;
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Prints the help texts associated with recognized flags and quits the
+///        program
+////////////////////////////////////////////////////////////////////////////////
 void cmdlinearguments::quit_with_help()
 {
-    GLOB_ARGS.print_help();
+    print_help();
     hila::finishrun();
 }
 
-cmdlinearguments GLOB_ARGS;
+////////////////////////////////////////////////////////////////////////////////
+/// @struct argmap_val
+/// @brief Struct to hold structured information on used command line arguments
+///
+/// @var strvec argmap_val::val_strings
+/// @brief Strings corresponding to read in cmdline parameters
+///
+/// @var std::string argmap_val::help_text
+/// @brief A string describing the use of the flag
+///
+/// @var bool argmap_val::used
+/// @brief A boolean telling whether the flag was found in argv
+////////////////////////////////////////////////////////////////////////////////
+struct argmap_val
+{
+    strvec val_strings;
+    std::string help_text;
+    bool used;
+};
 
-cmdlinearguments::cmdlinearguments() {
-    }
+// By importing cmdline 
+namespace hila {
+cmdlinearguments cmdline;
+}
 
+cmdlinearguments::cmdlinearguments() {}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Copies provided argc and argv into internal variables.
+///
+/// @param argc   length of argv
+/// @param argv   array of pointers to command-line arguments
+////////////////////////////////////////////////////////////////////////////////
 void cmdlinearguments::initialise_args(int argc0, char **argv0) {
     argc = argc0;
     argv = (const char **)malloc(argc * sizeof(const char *));
@@ -28,6 +59,12 @@ void cmdlinearguments::initialise_args(int argc0, char **argv0) {
         argv[i] = argv0[i];
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Returns the vector of strings associated with flag 'flag'.
+///
+/// @param flag   flag given as string
+/// @return vector of arguments
+////////////////////////////////////////////////////////////////////////////////
 strvec cmdlinearguments::values(std::string flag)
 {
     strvec valvec = argmap[flag].val_strings;
@@ -45,6 +82,17 @@ strvec cmdlinearguments::values(std::string flag)
     return valvec;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Adds a new recognized flag 'flag' and associated information to the
+///        argmap.
+///
+/// @param flag        string with the name of the flag, always of format "^-[a-z].*"
+///                    (no whitespace allowed), e.g. '-my_flag123'
+/// @param help_text   a string containing useful knowledge of the usage of the flag.
+///                    Will be printed when error occurs. For formatting, a newline
+///                    can be used to split the help_text, as it will help with
+///                    correct formatting in print_help().
+////////////////////////////////////////////////////////////////////////////////
 void cmdlinearguments::add_flag(std::string flag, std::string help_text)
 {
     if (argmap.count(flag) > 0)
@@ -60,6 +108,17 @@ void cmdlinearguments::add_flag(std::string flag, std::string help_text)
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Given flag '-flag' argv is scanned for valid entries that follow.
+/// @details Once '-flag' is found the following strings are scanned. If they
+///          are not in the format of a flag (described in add_flag()) they are
+///          considered valid input. argv with
+///          "-flag a -b_flag b -flag c d -flag -flag 4"
+///          should result in {"a", "c", "d", "4"}.
+///
+/// @param  flag   string corresponding to the flag
+/// @return Vector containing the found entries.
+////////////////////////////////////////////////////////////////////////////////
 strvec cmdlinearguments::read_arg_vector(const char *flag)
 {
     strvec uargs;
@@ -77,7 +136,7 @@ strvec cmdlinearguments::read_arg_vector(const char *flag)
         // check if its the flag
         if (std::strcmp(p, flag) == 0)
         {
-            // Indicate that the flag has been spotted on the command-line
+            // Indicate that the flag has been spotted in argv
             argmap[flag].used = true;
             // Slate for removal and move onto the
             // options
@@ -123,6 +182,9 @@ strvec cmdlinearguments::read_arg_vector(const char *flag)
     return uargs;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Loops over the known flags and fills the argmap struct from argv
+////////////////////////////////////////////////////////////////////////////////
 void cmdlinearguments::fill_argmap()
 {
     // Loop through the flags (keys of the map)
@@ -143,6 +205,10 @@ void cmdlinearguments::fill_argmap()
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Splits a help_text string into a vector of strings at newline.
+/// @return A vector containing the lines.
+////////////////////////////////////////////////////////////////////////////////
 strvec cmdlinearguments::parse_help(std::string help_text)
 {
     strvec lines;
@@ -157,9 +223,12 @@ strvec cmdlinearguments::parse_help(std::string help_text)
     return lines;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Prints the recognized flags and their help texts.
+////////////////////////////////////////////////////////////////////////////////
 void cmdlinearguments::print_help()
 {
-    hila::out0 << "Recognized command-line flags:\n";
+    hila::out0 << "Recognized command-line flags and their possible arguments:\n";
 
     for (auto const& p : argmap)
     {
@@ -177,6 +246,12 @@ void cmdlinearguments::print_help()
 
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Checks whether a flag has entries by returning the count. Does not
+///        differentiate on whether the flag is known.
+///
+/// @param flag
+////////////////////////////////////////////////////////////////////////////////
 int cmdlinearguments::flag_set(const char *flag)
 {
     if (argmap.count(flag) > 0)
@@ -184,11 +259,23 @@ int cmdlinearguments::flag_set(const char *flag)
     else return 0;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Checks if a flag has been found in argv
+///
+/// @return Boolean indicating the statement.
+////////////////////////////////////////////////////////////////////////////////
 bool cmdlinearguments::flag_used(const char *flag)
 {
     return argmap[flag].used;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Attempts to return a long integer from i:th entry of flag 'flag'.
+///        Failure results in terminating the program.
+/// @param flag
+/// @param i      index of the desired entry (default = 0);
+/// @return long conversion of the desired entry
+////////////////////////////////////////////////////////////////////////////////
 long cmdlinearguments::get_int(const char *flag, int i)
 {
     // If flag is found and non-empty
@@ -223,9 +310,18 @@ long cmdlinearguments::get_int(const char *flag, int i)
         hila::out0 << "Flag '" << flag << "' is missing an entry!\n";
         hila::out0 << "Terminating.\n";
         quit_with_help();
+        return -1;
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Attempts to return a double from i:th entry of flag 'flag'.
+///        Failure results in terminating the program. If std::stod
+///        doesn't complain, the conversion is considered valid.
+/// @param flag
+/// @param i      index of the desired entry (default = 0);
+/// @return double conversion of the desired entry
+////////////////////////////////////////////////////////////////////////////////
 double cmdlinearguments::get_double(const char *flag, int i)
 {
     // If flag is found and non-empty
@@ -267,6 +363,14 @@ double cmdlinearguments::get_double(const char *flag, int i)
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Attempts to return the i:th entry of flag 'flag'.
+///        Failure results in terminating the program.
+///
+/// @param flag
+/// @param i      index of the desired entry (default = 0);
+/// @return the desired entry
+////////////////////////////////////////////////////////////////////////////////
 std::string cmdlinearguments::get_string(const char *flag, int i)
 {
     int set = flag_set(flag);
