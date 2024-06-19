13ba3b372be2297c346a8ee3e58d4208b15ede24
Aaron Haarti <aaron.haarti@helsinki.fi>
2024-06-19 17:18:55 +0300

    surface visualisation works, need to add parabolic interpolation

--- applications/suN_gauge_twist/src/utility.h
+++ applications/suN_gauge_twist/src/utility.h
@@ -3,6 +3,11 @@
 
 #include "hila.h"
 #include <algorithm>
+
+enum class APPEND_FILE { TRUE, FALSE };
+enum class CLOSE_FILE { TRUE, FALSE };
+
+
 int z_ind(int z) {
     return (z + lattice.size(e_z)) % lattice.size(e_z);
 }
@@ -32,14 +37,39 @@ void print_formatted_numbers(const std::vector<Complex<T>> &numbers, std::string
 }
 
 template <typename T>
-void write_surface(std::vector<T> surf) {
-    std::ofstream MFile;
-    MFile.open("surface", std::ios_base::app);
-    hila::out0 << "OPENED surface file" << std::endl;
+void write_surface(std::vector<T> surf, std::string file_name, APPEND_FILE append_true,
+                   CLOSE_FILE close_file) {
+
+    static std::ofstream MFile; // Declare static ofstream object
+
+    // Open file if not already open, based on append_true parameter
+    if (!MFile.is_open()) {
+        if (append_true == APPEND_FILE::TRUE)
+            MFile.open(file_name, std::ios::app); // Append mode
+        else
+            MFile.open(file_name); // Default (overwrite) mode
+
+        if (!MFile.is_open()) {
+            std::cerr << "Error: Failed to open file " << file_name << std::endl;
+            return;
+        }
+
+        std::cout << "Opened file " << file_name << std::endl;
+    }
+    // MFile.open("surface", std::ios_base::app);
+    MFile << "volume: " << lattice.size(e_x) << " " << lattice.size(e_y) << " " << lattice.size(e_z)
+          << " " << lattice.size(e_t) << std::endl;
+    MFile << "x y z" << std::endl;
     for (int y = 0; y < lattice.size(e_y); y++) {
         for (int x = 0; x < lattice.size(e_x); x++) {
             if (hila::myrank() == 0)
-                MFile << x << ' ' << y << ' ' << surf[x + y * lattice.size(e_x)] << '\n';
+                MFile << x << ' ' << y << ' ' << surf[x + y * lattice.size(e_x)] << std::endl;
         }
     }
+    // Close file if specified by close_file parameter
+    if (close_file == CLOSE_FILE::TRUE) {
+        MFile.close();
+        std::cout << "Closed file " << file_name << std::endl;
+    }
+}
 #endif
