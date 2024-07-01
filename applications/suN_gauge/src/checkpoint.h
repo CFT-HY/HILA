d15ce722b54c5a5617267500a62038c20e3a92f1
Tobias Rindlisbacher <ritobias@gmx.ch>
2024-07-02 00:05:49 +0200

    applied clang format

--- applications/suN_gauge/src/checkpoint.h
+++ applications/suN_gauge/src/checkpoint.h
@@ -5,21 +5,22 @@
 #include "parameters.h"
 
 template <typename group>
-void checkpoint(const GaugeField<group>& U,int trajectory,const parameters& p) {
-    double t=hila::gettime();
+void checkpoint(const GaugeField<group> &U, int trajectory, const parameters &p) {
+    double t = hila::gettime();
     // name of config with extra suffix
-    std::string config_file=p.config_file+"_"+std::to_string(((trajectory+1)/p.n_save)%2);
+    std::string config_file =
+        p.config_file + "_" + std::to_string(((trajectory + 1) / p.n_save) % 2);
     // save config
     U.config_write(config_file);
     // write run_status file
-    if(hila::myrank()==0) {
+    if (hila::myrank() == 0) {
         std::ofstream outf;
-        outf.open("run_status",std::ios::out|std::ios::trunc);
-        outf<<"trajectory  "<<trajectory+1<<'\n';
-        outf<<"seed        "<<static_cast<uint64_t>(hila::random()*(1UL<<61))<<'\n';
-        outf<<"time        "<<hila::gettime()<<'\n';
+        outf.open("run_status", std::ios::out | std::ios::trunc);
+        outf << "trajectory  " << trajectory + 1 << '\n';
+        outf << "seed        " << static_cast<uint64_t>(hila::random() * (1UL << 61)) << '\n';
+        outf << "time        " << hila::gettime() << '\n';
         // write config name to status file:
-        outf<<"config name  "<<config_file<<'\n';
+        outf << "config name  " << config_file << '\n';
         outf.close();
     }
     std::stringstream msg;
@@ -28,32 +29,32 @@ void checkpoint(const GaugeField<group>& U,int trajectory,const parameters& p) {
 }
 
 template <typename group>
-bool restore_checkpoint(GaugeField<group>& U,int& trajectory,parameters& p) {
+bool restore_checkpoint(GaugeField<group> &U, int &trajectory, parameters &p) {
     uint64_t seed;
-    bool ok=true;
-    p.time_offset=0;
+    bool ok = true;
+    p.time_offset = 0;
     hila::input status;
-    if(status.open("run_status",false,false)) {
-        hila::out0<<"RESTORING FROM CHECKPOINT:\n";
-        trajectory=status.get("trajectory");
-        seed=status.get("seed");
-        p.time_offset=status.get("time");
+    if (status.open("run_status", false, false)) {
+        hila::out0 << "RESTORING FROM CHECKPOINT:\n";
+        trajectory = status.get("trajectory");
+        seed = status.get("seed");
+        p.time_offset = status.get("time");
         // get config name with suffix from status file:
-        std::string config_file=status.get("config name");
+        std::string config_file = status.get("config name");
         status.close();
         hila::seed_random(seed);
         U.config_read(config_file);
-        ok=true;
+        ok = true;
     } else {
         std::ifstream in;
-        in.open(p.config_file,std::ios::in|std::ios::binary);
-        if(in.is_open()) {
+        in.open(p.config_file, std::ios::in | std::ios::binary);
+        if (in.is_open()) {
             in.close();
-            hila::out0<<"READING initial config\n";
+            hila::out0 << "READING initial config\n";
             U.config_read(p.config_file);
-            ok=true;
+            ok = true;
         } else {
-            ok=false;
+            ok = false;
         }
     }
     return ok;
