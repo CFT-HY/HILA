c5d3f100b974f4051031a5212f14653eb120057e
Tobias Rindlisbacher <ritobias@gmx.ch>
2024-09-19 21:30:02 +0200

    added comments to stout_smear.h and switche from constant size c-array to std::vector to hold list of smeared fields per iteration, required for force computation.

--- libraries/gauge/stout_smear.h
+++ libraries/gauge/stout_smear.h
@@ -21,7 +21,7 @@ void sstaplesum(const GaugeField<T> &U, out_only Field<T> &staples, Direction d1
         U[d2].start_gather(d1, ALL);
         U[d1].start_gather(d2, ALL);
 
-        // calculate first lower 'U' of the staple sum
+        // calculate first lower 'u' of the staple sum
         // do it on opp parity
         onsites(ALL) {
             lower[X] = (U[d1][X] * U[d2][X + d1]).dagger() * U[d2][X];
@@ -87,8 +87,8 @@ void stout_smear(const GaugeField<T> &U, GaugeField<T> &stout, atype coeff, int 
 template <typename group, const int nstap = 2 * (NDIM - 1)>
 void get_stout_staples(const GaugeField<group> &U, Direction d1,
                           VectorField<group>(out_only &S)[nstap], out_only Field<group> &Ssum) {
-    // compute the plaquettes spanned by directions (d1,d2), where d2 runs through all positive and
-    // negative directions which are orthogonal to d1. Individual plauqette fields get stored in
+    // compute the staples spanned by directions (d1,d2), where d2 runs through all positive and
+    // negative directions which are orthogonal to d1. Individual staple fields get stored in
     // S[d1][d2'] and their sum over d2' in Ssum.
 
     foralldir(d2) if (d2 != d1) {
@@ -125,31 +125,33 @@ void get_stout_staples(const GaugeField<group> &U, Direction d1,
     }
 }
 
-template <typename T, int nst, typename atype = hila::arithmetic_type<T>>
-void stout_smear(const GaugeField<T> &U, GaugeField<T>(out_only &stoutlist)[nst],
-                 atype coeff) {
+template <typename T, typename atype = hila::arithmetic_type<T>>
+void stout_smear(const GaugeField<T> &U, out_only std::vector<GaugeField<T>> &stoutlist, atype coeff) {
     // performs nst stout smearing steps on the gauge field U and stores the gague field obtained
     // after the i-th smearing step in stoutlist[i]
     stoutlist[0] = U; // original field
-    for (int i = 1; i < nst; ++i) {
+    for (int i = 1; i < stoutlist.size(); ++i) {
         stout_smear1(stoutlist[i-1], stoutlist[i], coeff);
     }
 }
 
-template <typename T, int nst, int N = T::rows(), int NSTP = 2 * (NDIM - 1),
+template <typename T, int N = T::rows(), int NSTP = 2 * (NDIM - 1),
           typename atype = hila::arithmetic_type<T>>
-void stout_smear_force(const GaugeField<T> (&stoutlist)[nst], const VectorField<Algebra<T>> &K,
-                       out_only VectorField<Algebra<T>> &KS, atype coeff) {
+void stout_smear_force(const std::vector<GaugeField<T>> &stoutlist,
+                       const VectorField<Algebra<T>> &K, out_only VectorField<Algebra<T>> &KS,
+                       atype coeff) {
     // uses the list stoutlist[] of smeared gauge fields to compute the pullback of the
-    // algebra-valued force field K under the smearing and returs the force acting on the unsmeared
-    // link variables as algebra-valued field KS:
+    // algebra-valued force field K under the smearing and returns the force acting on the unsmeared
+    // link variables as algebra-valued field KS
+    // Note: our definition of the force field is different from the one used in [arXiv:hep-lat/0311018v1],
+    // in order to match the force field representation used by our HMC implementation.
     VectorField<T> stapl[NSTP];
     Field<T> staps;
     VectorField<T> K1;
 
     foralldir(d1) onsites(ALL) KS[d1][X] = K[d1][X];
 
-    for (int i = nst - 2; i >= 0; --i) {
+    for (int i = stoutlist.size() - 2; i >= 0; --i) {
         const GaugeField<T> &U0 = stoutlist[i + 1];
         const GaugeField<T> &U = stoutlist[i];
 
@@ -157,56 +159,73 @@ void stout_smear_force(const GaugeField<T> (&stoutlist)[nst], const VectorField<
             get_stout_staples(U, d1, stapl, staps);
             onsites(ALL) {
                 // compute stout smearing operator and its derivatives:
-                /*
-                T texp;
-                T dtexp[N][N];
-                T tplaq = U[d1][X] * staps[X];
-                chexp(tplaq.project_to_algebra_scaled(-coeff).expand(), texp, dtexp);
 
-                T m0 = texp.dagger() * KS[d1][X].expand();
-                T m1; // equivalent of Gamma matrix, eq. (74) in [arXiv:hep-lat/0311018v1]:
-                for (int ic1 = 0; ic1 < N; ++ic1) {
-                    for (int ic2 = 0; ic2 < N; ++ic2) {
-                        m1.e(ic1, ic2) = mul_trace(m0, dtexp[ic2][ic1]);
-                    }
-                }
-                // equivalent of Lambda matrix, eq. (73) in [arXiv:hep-lat/0311018v1]:
-                K1[d1][X] = m1.project_to_algebra_scaled(coeff).expand();
-
-                KS[d1][X] = (m0 * texp - tplaq * K1[d1][X]).project_to_algebra();
-                
-                K1[d1][X] *= U[d1][X];
-                */
+               // temp. variables:
                 T mtexp;
                 T mdtexp;
-                T tplaq = U[d1][X] * staps[X];
-                mult_chexp(tplaq.project_to_algebra_scaled(-coeff).expand(), KS[d1][X].expand(),
+                // turn staple sum into plaquette sum by multiplying with link variable:
+                T tplaqs = U[d1][X] * staps[X];
+                // the following function computes first for X = -coeff * tplaqs the smearing
+                // operator Q = exp(X) and its derivatives dQ/dX[][], and uses these to
+                // compute the two matrices: 
+                // mtexp = Q.dagger() * KS[d1][X].expand() * Q 
+                // and 
+                // mdtexp[i][j] = trace(Q.dagger * KS[d1][X].expand() * dQ/dX[j][i]) :
+                mult_chexp(tplaqs.project_to_algebra_scaled(-coeff).expand(), KS[d1][X].expand(),
                            mtexp, mdtexp);
+                
+                // set K1[d1][X] to be the equivalent of the \Lambda matrix from eq.(73) in
+                // [arXiv:hep-lat/0311018v1]:
                 K1[d1][X] = mdtexp.project_to_algebra_scaled(coeff).expand();
 
-                KS[d1][X] = (mtexp - tplaq * K1[d1][X]).project_to_algebra();
+                // equivalent of first line and first term on second line of eq.(75) in
+                // [arXiv:hep-lat/0311018v1]:
+                KS[d1][X] = (mtexp - tplaqs * K1[d1][X]).project_to_algebra();
 
+                // multiply K1[d1] by U[d1]:
                 K1[d1][X] *= U[d1][X];
+                // (the latter is done so that the parallel transport of
+                // U[d1][X] * stapl[][d1][X] * K1[d1][X] in negative d1-direction, required
+                // in the next section, can be computed simply as stapl[][d1][X-d1] * K1[d1][X-d1]
             }
-            K1[d1].start_gather(-d1, ALL);
-        }
-        foralldir(d1) {
+
+            // multiply all the stapl[][d1] by K1[d1] and start sending the resulting matrices
+            // to the neighboring sites in negative d1-direction: 
+            // (this is done in the same order in which they will be used below)
             int istp = 0;
+            int istn;
             foralldir(d2) if (d2 != d1) {
-                // d1-d2-plaquette for positive d2-direction:
+                onsites(ALL) stapl[istp][d1][X] *= K1[d1][X];
+                stapl[istp][d1].start_gather(-d1, ALL);
+                istn = NSTP - 1 - istp;
+                onsites(ALL) stapl[istn][d1][X] *= K1[d1][X];
+                stapl[istn][d1].start_gather(-d1, ALL);
+                ++istp;
+            }
+        }
+
+        foralldir(d1) {
+            // compute the equivalent of the terms in the curly bracket of eq.(75) in
+            // [arXiv:hep-lat/0311018v1]:
+            int istp = 0;
+            int istn;
+            foralldir(d2) if (d2 != d1) {
+                // term from d1-d2-plaquette for positive d2-direction:
                 onsites(ALL) {
-                    staps[X] = stapl[istp][d1][X - d1] * K1[d1][X - d1];
+                    staps[X] = stapl[istp][d1][X - d1];
                 }
-                // define for each stout staple matrix the correspondin path of gauge links
+                // define for each modified stout staple matrix the correspondin path of gauge
+                // links:
                 std::vector<Direction> pathp = {d2, -d1, -d2};
                 get_wloop_force_from_wl_add(U, pathp, staps, 1.0, KS);
 
-                // d1-d2-plaquette for negative d2-direction:
-                int istn = NSTP - 1 - istp;
+                // term from d1-d2-plaquette for negative d2-direction:
+                istn = NSTP - 1 - istp;
                 onsites(ALL) {
-                    staps[X] = stapl[istn][d1][X - d1] * K1[d1][X - d1];
+                    staps[X] = stapl[istn][d1][X - d1];
                 }
-                // define for each stout staple matrix the correspondin path of gauge links
+                // define for each modified stout staple matrix the correspondin path of gauge
+                // links:
                 std::vector<Direction> pathn = {-d2, -d1, d2};
                 get_wloop_force_from_wl_add(U, pathn, staps, 1.0, KS);
                 ++istp;
@@ -214,102 +233,5 @@ void stout_smear_force(const GaugeField<T> (&stoutlist)[nst], const VectorField<
         }
     }
 }
-
-/*
-
-template <typename group, const int nstap=2*(NDIM-1)>
-void get_stout_plaquettes(const GaugeField<group> &U, Direction d1,
-                       VectorField<group>(out_only &S)[nstap], out_only Field<group> &Ssum) {
-    // compute the plaquettes spanned by directions (d1,d2), where d2 runs through all positive and
-    // negative directions which are orthogonal to d1. Individual plauqette fields get stored in
-    // S[d1][d2'] and their sum over d2' in Ssum.
-
-    foralldir(d2) if (d2 != d1) {
-        U[d2].start_gather(d1, ALL);
-        U[d1].start_gather(d2, ALL);
-    }
-    onsites(ALL){
-        Ssum[X] = 0;
-    }
-    Field<group> tF;
-
-    int k = 0;
-    int rk;
-    foralldir(d2) if (d2 != d1) {
-        onsites(ALL) {
-            tF[X] = U[d1][X + d2] * (U[d1][X] * U[d2][X + d1]).dagger();
-            // d1-d2-plaquette with positive d2 that starts and ends at X
-            S[k][d1][X] = (U[d2][X] * tF[X]).dagger();
-            tF[X] = tF[X] * U[d2][X];
-        }
-        tF.start_gather(-d2, ALL);
-        onsites(ALL) {
-            Ssum[X] += S[k][d1][X];
-        }
-        rk = nstap - 1 - k;
-        onsites(ALL) {
-            // d1-d2-plaquette with negative d2 that starts and ends at X
-            S[rk][d1][X] = tF[X - d2];
-            Ssum[X] += S[rk][d1][X];
-        }
-        ++k;
-    }
-}
-
-template <typename T, int nst, int N = T::rows(), int NSTP = 2 * (NDIM - 1),
-          typename atype = hila::arithmetic_type<T>>
-void stout_smear_force(const GaugeField<T> (&stoutlist)[nst], const VectorField<Algebra<T>> &K,
-                       out_only VectorField<Algebra<T>> &KS, atype coeff) {
-    // uses the list stoutlist[] of smeared gauge fields to compute the pullback of the
-    // algebra-valued force field K under the smearing and returs the force acting on the unsmeared
-    // link variables as algebra-valued field KS:
-    VectorField<T> plaql[NSTP];
-    Field<T> plaqs;
-    VectorField<T> K1;
-
-    foralldir(d1) onsites(ALL) KS[d1][X] = K[d1][X];
-
-    for (int i = nst - 2; i >= 0; --i) {
-        const GaugeField<T> &U0 = stoutlist[i + 1];
-        const GaugeField<T> &U = stoutlist[i];
-
-        foralldir(d1) {
-            get_stout_plaquettes(U,d1,plaql,plaqs);
-            onsites(ALL) {
-                // compute stout smearing operator and its derivatives:
-                T mtexp;
-                T mdtexp;
-                mult_chexp(plaqs[X].project_to_algebra_scaled(-coeff).expand(), KS[d1][X].expand(),
-                           mtexp, mdtexp);
-                KS[d1][X] = mtexp.project_to_algebra();
-                K1[d1][X] = mdtexp.project_to_algebra_scaled(coeff).expand();
-            }
-        }
-        foralldir(d1) {
-            int istp = 0;
-            foralldir(d2) if (d2 != d1) {
-                // d1-d2-plaquette for positive d2-direction:
-                 onsites(ALL) {
-                    plaqs[X] = plaql[istp][d1][X] * K1[d1][X];
-                }
-                // define for each stout plaquette matrix the correspondin path of gauge links
-                std::vector<Direction> pathp = {d1, d2, -d1, -d2};
-                get_wloop_force_from_wl_add(U, pathp, plaqs, 1.0, KS);
-
-                // d1-d2-plaquette for negative d2-direction:
-                int istn = NSTP - 1 - istp;
-                onsites(ALL) {
-                    plaqs[X] = plaql[istn][d1][X] * K1[d1][X];
-                }
-                // define for each stout plaquette matrix the correspondin path of gauge links
-                std::vector<Direction> pathn = {d1, -d2, -d1, d2};
-                get_wloop_force_from_wl_add(U, pathn, plaqs, 1.0, KS);
-                ++istp;
-            }
-        }
-
-    }
-}
-*/
 
 #endif
