980749ac98a20dfadcd8b1ea469f83c6a43fa502
timohyva <hyvarinentimo00@gmail.com>
2025-01-08 02:32:41 +0200

    The ifeq block for lumi-hip-cc arch is removed from libraries/main.mk CXXFLAGS of *.o rule is moved to just before *.cpt source file. In this way, a general rule for all ARCH is kept, both LUMI-G and LUMI-C are tested.
    AFter this, -xhip then can be put into last position of CXXFLAGS of  lumi-hip-CC.mk.
    
    LDFLAGS in lumi-hip-CC.mk is cleaned off. -fgpu-rdc is removed because it isn't about linking.
    _HIP_PLATFORM_HCC_ is changed to _HIP_PLATFORM_AMD_.
    libraries flags -lamdhip64 -lhipfft -lmpi are grouped into LDLIBS for good practice.
    
    Thoese change have succesfully tested on LUMI with two AMD MI250X GPU-aware-mpi.

--- libraries/main.mk
+++ libraries/main.mk
@@ -199,14 +199,8 @@ build/%.cpt: %.cpp Makefile $(MAKEFILE_LIST) $(ALL_DEPEND) $(APP_HEADERS)
 	@mkdir -p build
 	$(HILAPP) $(HILAPP_OPTS) $(APP_OPTS) $(HILA_OPTS) $< -o $@ $(HILAPP_TRAILING_OPTS)
 
-# Clang's AMD GPU support option -xhip should be put just before *.cpt source file.
-ifeq ($(ARCH), lumi-hip-CC)
 build/%.o : build/%.cpt
-	$(CC) $(CXXFLAGS) $(APP_OPTS) $(HILA_OPTS) -xhip $< -c -o $@
-else
-build/%.o : build/%.cpt
-	$(CC) $(CXXFLAGS) $(APP_OPTS) $(HILA_OPTS) $< -c -o $@
-endif
+	$(CC) $(APP_OPTS) $(HILA_OPTS) $(CXXFLAGS) $< -c -o $@
 
 build/%.cpt: $(LIBRARIES_DIR)/plumbing/%.cpp $(ALL_DEPEND) $(HILA_HEADERS)
 	@mkdir -p build
@@ -215,7 +209,6 @@ build/%.cpt: $(LIBRARIES_DIR)/plumbing/%.cpp $(ALL_DEPEND) $(HILA_HEADERS)
 build/%.cpt: $(LIBRARIES_DIR)/tools/%.cpp $(ALL_DEPEND) $(HILA_HEADERS)
 	@mkdir -p build
 	$(HILAPP) $(HILAPP_OPTS) $(APP_OPTS) $(HILA_OPTS) $< -o $@ $(HILAPP_TRAILING_OPTS)
-
 
 # This one triggers only for cuda targets
 build/%.cpt: $(LIBRARIES_DIR)/plumbing/backend_gpu/%.cpp $(ALL_DEPEND) $(HILA_HEADERS)
