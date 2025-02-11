8e61c2123ecc5747661304cc6b77e02fead22646
Aaron Haarti <aaron.haarti@helsinki.fi>
2025-02-11 13:50:26 +0200

    added output folder option for surface measurement

--- applications/suN_gauge_twist/src/polyakov_surface.h
+++ applications/suN_gauge_twist/src/polyakov_surface.h
@@ -398,14 +398,14 @@ void measure_polyakov_surface(GaugeField<group> &U, const parameters &p, int tra
             // spectraldensity_surface(surf2, npow, hits);
 
             if (traj == p.n_trajectories - 1) {
-                write_fourier(npow, hits, pow_size, "fourier_profile_" + std::to_string(smear),
+                write_fourier(npow, hits, pow_size, p.out_folder + "fourier_profile_" + std::to_string(smear),
                               APPEND_FILE::TRUE, CLOSE_FILE::TRUE);
-                write_surface(surf_interpolated, "surface_smooth_" + std::to_string(smear),
+                write_surface(surf_interpolated, p.out_folder + "surface_smooth_" + std::to_string(smear),
                               APPEND_FILE::TRUE, CLOSE_FILE::TRUE);
             } else {
-                write_fourier(npow, hits, pow_size, "fourier_profile_" + std::to_string(smear),
+                write_fourier(npow, hits, pow_size, p.out_folder + "fourier_profile_" + std::to_string(smear),
                               APPEND_FILE::TRUE, CLOSE_FILE::FALSE);
-                write_surface(surf_interpolated, "surface_smooth_" + std::to_string(smear),
+                write_surface(surf_interpolated, p.out_folder + "surface_smooth_" + std::to_string(smear),
                               APPEND_FILE::TRUE, CLOSE_FILE::FALSE);
             }
         }
