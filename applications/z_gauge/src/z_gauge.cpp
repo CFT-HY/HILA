471b2dea16a91b215cc92027a3b3f4632a0cf426
Tobias Rindlisbacher <ritobias@gmx.ch>
2025-02-20 22:12:20 +0200

    fixed a bug and balanced plaquette shift and gauge link updates a bit better. added additional observables.

--- applications/z_gauge/src/z_gauge.cpp
+++ applications/z_gauge/src/z_gauge.cpp
@@ -15,7 +15,7 @@ struct parameters {
     int n_traj;         // number of trajectories to generate
     int n_therm;        // number of thermalization trajectories (counts only accepted traj.)
     int n_update;       // number of heat-bath sweeps per "trajectory"
-    int n_relax;        // number of s_{x,\mu\nu} updates per update
+    int n_ps_update;    // number of plaquette shift (s_{x,\mu\nu}-vars) updates per update
     int n_save; // number of trajectories between config. check point
     std::string config_file;
     ftype time_offset;
@@ -139,36 +139,6 @@ fT sw_metropolis(fT &sw,const T &tplaq, double beta) {
 }
 
 /**
- * @brief Wrapper function to update plaquette shift variables
- * @details --
- *
- * @tparam fT shift type such as double or float
- * @tparam T Group element type such as long or int
- * @param sw plaquette shift variable
- * @param p paramaters
- * @param H GaugeField from which to compute the plaquettes
- * @return double change in plaquette action
- */
-template <typename fT, typename T>
-void update_relax(sw_t<fT> &sw, const parameters &p, const GaugeField<T> &H) {
-    static hila::timer rl_timer("Relax");
-
-    rl_timer.start();
-
-    foralldir(d1) foralldir(d2) if (d2 > d1) {
-        H[d2].start_gather(d1, ALL);
-        H[d1].start_gather(d2, ALL);
-        onsites(ALL) {
-            T tplaq = H[d1][X] + H[d2][X + d1] - H[d1][X + d2] - H[d2][X];
-            sw_metropolis(sw[d1][d2][X], tplaq, p.beta);
-        }
-    }
-
-    rl_timer.stop();
-
-}
-
-/**
  * @brief Wrapper function to update GaugeField per direction
  * @details Computes first staplesum, then uses computed result to evolve GaugeField with Metropolis
  * updates
@@ -184,7 +154,7 @@ void update_relax(sw_t<fT> &sw, const parameters &p, const GaugeField<T> &H) {
 template <typename T, typename fT>
 void update_parity_dir(GaugeField<T> &H, const parameters &p, Parity par, Direction d, const sw_t<fT> &sw) {
 
-    static hila::timer me_timer("Metropolis");
+    static hila::timer me_timer("Metropolis (z)");
     static hila::timer staples_timer("Staplesum");
 
     Field<fT> staples;
@@ -204,6 +174,37 @@ void update_parity_dir(GaugeField<T> &H, const parameters &p, Parity par, Direct
 }
 
 /**
+ * @brief Wrapper function to update plaquette shift variables
+ * @details --
+ *
+ * @tparam fT shift type such as double or float
+ * @tparam T Group element type such as long or int
+ * @param sw plaquette shift variable
+ * @param p paramaters
+ * @param H GaugeField from which to compute the plaquettes
+ * @return double change in plaquette action
+ */
+template <typename fT, typename T>
+void update_sw_parity_dir(sw_t<fT> &sw, const parameters &p, Parity par, Direction d1,
+                          const GaugeField<T> &H) {
+    static hila::timer sw_timer("Metropolis (ps)");
+
+    sw_timer.start();
+
+    foralldir(d2) if (d2 != d1) {
+        H[d2].start_gather(d1, par);
+        H[d1].start_gather(d2, par);
+        onsites(par) {
+            T tplaq = H[d1][X] + H[d2][X + d1] - H[d1][X + d2] - H[d2][X];
+            sw_metropolis(sw[d1][d2][X], tplaq, p.beta);
+            sw[d2][d1][X] = -sw[d1][d2][X];
+        }
+    }
+
+    sw_timer.stop();
+}
+
+/**
  * @brief Wrapper update function
  * @details Gauge Field update sweep with randomly chosen parities and directions
  *
@@ -217,14 +218,17 @@ template <typename T, typename fT>
 void update(GaugeField<T> &H, sw_t<fT> &sw, const parameters &p) {
 
     for (int i = 0; i < 2 * NDIM; ++i) {
-        bool relax = hila::broadcast((int)(hila::random() * (1 + p.n_relax)) != 0);
-        if(relax) {
-            update_relax(sw, p, H);
+        bool do_sw = hila::broadcast((int)(hila::random() * (1 + p.n_ps_update)) != 0);
+
+        int tdp = hila::broadcast((int)(hila::random() * 2 * NDIM));
+        int tdir = tdp / 2;
+        int tpar = 1 + (tdp % 2);
+        // hila::out0 << "   " << Parity(tpar) << " -- " << Direction(tdir);
+        if (do_sw) {
+            // update plaquette shift variables for plaquettes spanned by (x,\mu\nu) = (x_{tpar},tdir,d2), d2!=tdir 
+            update_sw_parity_dir(sw, p, Parity(tpar), Direction(tdir), H);
         } else {
-            int tdp = hila::broadcast((int)(hila::random() * 2 * NDIM));
-            int tdir = tdp / 2;
-            int tpar = 1 + (tdp % 2);
-            // hila::out0 << "   " << Parity(tpar) << " -- " << Direction(tdir);
+            // update Z-gauge variable on links (x,\mu) = (x_{tpar},tdir)
             update_parity_dir(H, p, Parity(tpar), Direction(tdir), sw);
         }
     }
@@ -244,7 +248,7 @@ void update(GaugeField<T> &H, sw_t<fT> &sw, const parameters &p) {
 template <typename T, typename fT>
 void do_trajectory(GaugeField<T> &H, sw_t<fT> &sw, const parameters &p) {
     for (int n = 0; n < p.n_update; n++) {
-        for (int i = 0; i <= p.n_relax; i++) {
+        for (int i = 0; i <= p.n_ps_update; i++) {
             update(H, sw, p);
         }
     }
@@ -271,6 +275,45 @@ double measure_s_plaq(const GaugeField<T> &H, const sw_t<fT> &sw) {
     return plaq.value();
 }
 
+template <typename T>
+double measure_ns_plaq(const GaugeField<T> &H) {
+    // measure the total plaquette action for the gauge field H
+    Reduction<double> plaq = 0;
+    plaq.allreduce(false).delayed(true);
+    foralldir(d1) foralldir(d2) if (d1 < d2) {
+        H[d2].start_gather(d1, ALL);
+        H[d1].start_gather(d2, ALL);
+        onsites(ALL) {
+            plaq += pow((double)(H[d1][X] + H[d2][X + d1] - H[d1][X + d2] - H[d2][X]),
+                        2.0);
+        }
+    }
+    return plaq.value();
+}
+
+template <typename T>
+double measure_hsq(const GaugeField<T> &H, double(out_only &h_per_par_dir)[2][NDIM]) {
+    // measure the total plaquette action for the gauge field H
+    Reduction<double> thsq = 0;
+    thsq.allreduce(false).delayed(true);
+    ReductionVector<double> h_per_p_d(2 * NDIM);
+    h_per_p_d = 0.0;
+    h_per_p_d.allreduce(false).delayed(true);
+    foralldir(d1) {
+        onsites(ALL) {
+            thsq += pow(H[d1][X], 2.0);
+            h_per_p_d[((int)X.parity() - 1) * NDIM + d1] += H[d1][X];
+        }
+    }
+    h_per_p_d.reduce();
+    for (int par = 0; par < 2; ++par) {
+        for (int dir = 0; dir < NDIM; ++dir) {
+            h_per_par_dir[par][dir] = h_per_p_d[par*NDIM+dir];
+        }
+    }
+    return thsq.value();
+}
+
 template <typename T, typename fT>
 void measure_stuff(const GaugeField<T> &H, const sw_t<fT> &sw) {
     // perform measurements on current gauge field H and
@@ -278,11 +321,27 @@ void measure_stuff(const GaugeField<T> &H, const sw_t<fT> &sw) {
     static bool first = true;
     if (first) {
         // print legend for measurement output
-        hila::out0 << "LMEAS:        plaq\n";
+        hila::out0 << "LMEAS:       splaq        nsplaq           hsq";
+        for (int par = 0; par < 2; ++par) {
+            for (int dir = 0; dir < NDIM; ++dir) {
+                hila::out0 << "          p" << par << "d" << dir;
+            }
+        }
+        hila::out0 << "\n";
         first = false;
     }
-    auto plaq = measure_s_plaq(H, sw) / (lattice.volume() * NDIM * (NDIM - 1) / 2);
-    hila::out0 << string_format("MEAS % 0.6e", plaq) << '\n';
+    auto splaq = measure_s_plaq(H, sw) / (lattice.volume() * NDIM * (NDIM - 1) / 2);
+    auto nsplaq = measure_ns_plaq(H) / (lattice.volume() * NDIM * (NDIM - 1) / 2);
+    double h_per_par_dir[2][NDIM];
+    auto hsq = measure_hsq(H, h_per_par_dir) / (lattice.volume() * NDIM);
+    hila::out0 << string_format("MEAS % 0.6e % 0.6e % 0.6e", splaq, nsplaq, hsq);
+    for (int par = 0; par < 2; ++par) {
+        for (int dir = 0; dir < NDIM; ++dir) {
+            h_per_par_dir[par][dir] /= lattice.volume() / 2;
+            hila::out0 << string_format(" % 0.6e", h_per_par_dir[par][dir]);
+        }
+    }
+    hila::out0 << '\n';
 }
 
 // end measurement functions
@@ -383,7 +442,7 @@ int main(int argc, char **argv) {
     // number of Metropolis sweeps per trajectory
     p.n_update = par.get("updates in trajectory");
     // number of s_{x,\mu\nu} relaxation sweeps per Metropolis sweep
-    p.n_relax = par.get("relax steps");
+    p.n_ps_update = par.get("plaq shift updates");
     // number of thermalization trajectories
     p.n_therm = par.get("thermalization trajs");
     // random seed = 0 -> get seed from time
@@ -413,13 +472,14 @@ int main(int argc, char **argv) {
                     sw[d1][d2][X] = 0;
                 } else {
                     sw[d1][d2][X] = 0.5;
-                    
-                    if(X.parity() == Parity::odd) {
-                        sw[d1][d2][X] = -sw[d1][d2][X];
-                    }
-                    if(1) {
-                        if (((int)d1+(int)d2) % 2 == 1) {
+                    if (1) {
+                        if (X.parity() == Parity::odd) {
                             sw[d1][d2][X] = -sw[d1][d2][X];
+                        }
+                        if(1) {
+                            if (((int)d1+(int)d2) % 2 == 1) {
+                                sw[d1][d2][X] = -sw[d1][d2][X];
+                            }
                         }
                     }
                     sw[d2][d1][X] = -sw[d1][d2][X];
