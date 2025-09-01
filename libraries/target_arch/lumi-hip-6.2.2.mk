a60f63a6ac4d83e2189486043c918989bf5303b0
haaaaron <aaron.haarti@protonmail.com>
2025-09-01 16:16:03 +0300

    cleaned up lumi makefiles for rocm 6.2.2 now sould also support static build for hilapp with HILAPP_INCLUDES list

--- libraries/target_arch/lumi-hip-6.2.2.mk
+++ libraries/target_arch/lumi-hip-6.2.2.mk
@@ -1,6 +1,11 @@
 $(info ########################################################################)
 $(info Target lumi-hip-6.2.2: remember to )
+$(info Clang compilers)
 $(info   module load LUMI/24.03 partition/G  craype-accel-amd-gfx90a PrgEnv-amd cray-fftw rocm/6.2.2 )
+$(info GNU compilers)
+$(info   module load LUMI/24.03 partition/G cpeGNU/24.03  craype-accel-amd-gfx90a cray-fftw rocm/6.2.2)
+$(info If using singularity container for hilapp remember to:)
+$(info     export SINGULARITY_BIND="{HILA_INSTALL_PATH}")
 $(info ########################################################################)
 
 
@@ -41,26 +46,27 @@ CXXFLAGS += $(foreach dir,$(GCC_CXX_INCLUDES),-isystem $(dir))
 # is inface Clang 17.0.1, whcih know the pathes of Clang c++ 17 std include files.
 # Then the good practice to get HILAPP_INCLUDE_LIST is call CC -xc++ --std=c++17 -Wp,-v - otherthan us g++
 
-# HILAPP_INCLUDE_LIST := $(addprefix -I, $(shell echo | g++ -xc++ --std=c++17 -Wp,-v - 2>&1 | grep "^ "))
-#HILAPP_INCLUDE_LIST := $(addprefix -I, $(shell echo | CC -xc++ --std=c++20 --stdlib=libstdc++ -Wp,-v - 2>&1 | grep "^ "))
+HILAPP_INCLUDE_LIST := $(addprefix -I, $(shell echo | CC -xc++ --std=c++17 -Wp,-v - 2>&1 | grep "^ "))
 
 # stddef.h again!
 # HILAPP_INCLUDE_LIST += -I/opt/cray/pe/gcc/default/snos/lib/gcc/x86_64-suse-linux/default/include -I/opt/cray/pe/fftw/default/x86_64/include
-#HILAPP_INCLUDE_LIST += -I/opt/cray/pe/fftw/default/x86_64/include
+HILAPP_INCLUDE_LIST += -I/opt/cray/pe/fftw/default/x86_64/include
 
 # Write hilapp inlcudes to a file 0hilapp_incl_dirs
-#$(shell mkdir -p build)
-#$(shell echo "$(HILAPP_INCLUDE_LIST)" > build/0hilapp_incl_dirs )
-#HILAPP_INCLUDES := `cat build/0hilapp_incl_dirs`
+$(shell mkdir -p build)
+$(shell echo "$(HILAPP_INCLUDE_LIST)" > build/0hilapp_incl_dirs )
+HILAPP_INCLUDES := `cat build/0hilapp_incl_dirs`
 
 HILA_OBJECTS += build/hila_gpu.o build/memory_pool.o
 
 # ROCM_LIBS := $(shell echo ${ROCM_PATH} | sed s/rocm/rocmlibs/)
 
 # Currently LUMI EAP things require explicit setting of many include paths -- NOTE: MPI may be bad
-HILA_INCLUDES := -I${ROCM_PATH}/hip/include -I${ROCM_PATH}/hip/include/hip
-HILA_INCLUDES += -I${ROCM_PATH}/rocrand/include/ -I${ROCM_PATH}/hiprand/include/ 
-HILA_INCLUDES += -I${ROCM_PATH}/hipfft/include/ -I${ROCM_PATH}/hipcub/include/hipcub
+HILA_INCLUDES := -I${ROCM_PATH}/include
+HILA_INCLUDES += -I${ROCM_PATH}/include/hip -I${ROCM_PATH}/include/hip
+HILA_INCLUDES += -I${ROCM_PATH}/include/rocrand -I${ROCM_PATH}/include/hiprand
+HILA_INCLUDES += -I${ROCM_PATH}/include/hipfft -I${ROCM_PATH}/include/hipcub
+HILA_INCLUDES += -I${ROCM_PATH}/include/roctracer
 HILA_INCLUDES += -I${MPICH_DIR}/include -I/opt/cray/pe/fftw/default/x86_64/include
 #HILA_INCLUDES += -I/opt/cray/pe/cce/17.0.1/cce/x86_64/include/craylibs
 # HILA_INCLUDES += -I${MPICH_DIR}/include
