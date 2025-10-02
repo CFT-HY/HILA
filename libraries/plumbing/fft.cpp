181a39a47bdee43c12de95a2b30293fc3d5a9abf
Kari Rummukainen <rummukai@dx5-528-33208.ad.helsinki.fi>
2025-10-02 22:29:44 +0300

    fft: make fft variables fixed to lattice - each lattice now can do its own fft. TODO: make GPU fft to use the same memory area!

--- libraries/plumbing/fft.cpp
+++ libraries/plumbing/fft.cpp
@@ -17,11 +17,6 @@ hila::timer pencil_save_timer(" save pencils");
 
 hila::timer binning_timer("bin field time");
 
-// a couple of global fft variables - easiest this way
-std::vector<pencil_struct> hila_pencil_comms[NDIM];
-unsigned hila_fft_my_columns[NDIM]; // how many columns does this node take care of
-size_t pencil_recv_buf_size[NDIM];
-
 // static variable to hold fft plans
 #if (defined(HIP) || defined(CUDA)) && !defined(HILAPP)
 hila_saved_fftplan_t hila_saved_fftplan;
@@ -44,10 +39,11 @@ size_t pencil_get_buffer_offsets(const Direction dir, const size_t elements,
     size_t element_offset = lattice->mynode.size[dir];
     size_t s = element_offset * elements;
 
-    foralldir(d) if (d != dir) {
-        offset[d] = s;
-        s *= lattice->mynode.size[d];
-    }
+    foralldir (d)
+        if (d != dir) {
+            offset[d] = s;
+            s *= lattice->mynode.size[d];
+        }
 
     return element_offset;
 }
@@ -59,18 +55,16 @@ size_t pencil_get_buffer_offsets(const Direction dir, const size_t elements,
 
 void init_pencil_direction(Direction dir) {
 
-    static lattice_struct *fftlat = nullptr;
-    if (fftlat == nullptr)
-        fftlat = lattice.ptr();
-    else if (fftlat != lattice.ptr()) {
-        hila::out0 << "FFT ERROR: different lattice size!\n";
-        hila::terminate(0);
+    if (lattice->fftdata == nullptr) {
+        lattice.ptr()->fftdata = new hila::fftdata_struct;
     }
+    
+    hila::fftdata_struct &fft = *(lattice.ptr()->fftdata);
 
-    if (hila_pencil_comms[dir].size() == 0) {
+    if (fft.hila_pencil_comms[dir].size() == 0) {
         // basic structs not yet set, do it here
 
-        hila_pencil_comms[dir].resize(lattice->nodes.n_divisions[dir]);
+        fft.hila_pencil_comms[dir].resize(lattice->nodes.n_divisions[dir]);
 
         int nodenumber = 0;
         for (int nodenumber = 0; nodenumber < lattice->nodes.number; ++nodenumber) {
@@ -78,20 +72,21 @@ void init_pencil_direction(Direction dir) {
             const node_info n = lattice->nodes.nodeinfo(nodenumber);
 
             bool is_in_column = true;
-            foralldir(d) if (d != dir && n.min[d] != lattice->mynode.min[d]) {
-                is_in_column = false;
-                break; // breaks out of foralldir
-            }
+            foralldir (d)
+                if (d != dir && n.min[d] != lattice->mynode.min[d]) {
+                    is_in_column = false;
+                    break; // breaks out of foralldir
+                }
 
             // store the nodes in the hila_pencil_comms -list in the right order -
             // nodes may be reordered by some weird layout
             if (is_in_column) {
-                pencil_struct fn;
+                hila::pencil_struct fn;
                 fn.node = nodenumber;
                 fn.size_to_dir = n.size[dir];
                 for (int i = 0; i < lattice->nodes.n_divisions[dir]; i++) {
                     if (n.min[dir] == lattice->nodes.divisors[dir][i]) {
-                        hila_pencil_comms[dir].at(i) = fn;
+                        fft.hila_pencil_comms[dir].at(i) = fn;
                     }
                 }
             }
@@ -99,29 +94,29 @@ void init_pencil_direction(Direction dir) {
 
         size_t total_columns = lattice->mynode.volume / lattice->mynode.size[dir];
 
-        size_t nodes = hila_pencil_comms[dir].size();
+        size_t nodes = fft.hila_pencil_comms[dir].size();
 
         // column offset and number are used for sending
         size_t i = 0;
-        for (pencil_struct &fn : hila_pencil_comms[dir]) {
+        for (auto &fn : fft.hila_pencil_comms[dir]) {
             fn.column_offset = ((i * total_columns) / nodes) * lattice->mynode.size[dir];
             fn.column_number =
                 (((i + 1) * total_columns) / nodes) - fn.column_offset / lattice->mynode.size[dir];
 
             if (fn.node == hila::myrank()) {
-                hila_fft_my_columns[dir] = fn.column_number;
+                fft.hila_fft_my_columns[dir] = fn.column_number;
             }
             i++;
         }
 
-        pencil_recv_buf_size[dir] = 0;
+        fft.pencil_recv_buf_size[dir] = 0;
 
-        for (pencil_struct &fn : hila_pencil_comms[dir]) {
-            fn.recv_buf_size = hila_fft_my_columns[dir] * fn.size_to_dir;
+        for (auto &fn : fft.hila_pencil_comms[dir]) {
+            fn.recv_buf_size = fft.hila_fft_my_columns[dir] * fn.size_to_dir;
 
             // how big array?
             if (fn.node != hila::myrank())
-                pencil_recv_buf_size[dir] += fn.recv_buf_size;
+                fft.pencil_recv_buf_size[dir] += fn.recv_buf_size;
         }
 
     } // setup
