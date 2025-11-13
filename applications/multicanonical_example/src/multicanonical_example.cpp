56a1ae6fdfad9e047f97d55977d52ab3c0f69b83
Jaakko Annala <jgannala@gmail.com>
2025-11-13 23:06:21 +0200

    edit muca example to work with changes

--- applications/multicanonical_example/src/multicanonical_example.cpp
+++ applications/multicanonical_example/src/multicanonical_example.cpp
@@ -15,35 +15,39 @@
 
 static_assert(NDIM == 3, "NDIM must be 3 here");
 
-Field<double> compute_local_action(Field<double> phi);
-void multican_update(Field<double> &phi);
-double order_parameter(Field<double> &phi);
-void iterate_weights(Field<double> &phi);
-
 // For the action we take a simple real scalar field with a
 // potential with two degenerate minima.
 Field<double> compute_local_action(Field<double> phi) {
     Field<double> action;
-    onsites(ALL) {
+    onsites (ALL) {
         action[X] = -pow(phi[X], 2) + pow(phi[X], 4);
     }
-    foralldir(d) onsites(ALL) {
-        action[X] += pow(phi[X] - phi[X - d], 2);
-        action[X] += pow(phi[X + d] - phi[X], 2);
-    }
+    foralldir (d)
+        onsites (ALL) {
+            action[X] += pow(phi[X] - phi[X - d], 2);
+            action[X] += pow(phi[X + d] - phi[X], 2);
+        }
     return action;
+}
+
+// Mean value of phi is a convenient order parameter, since its cheap
+// to compute and has different values in the different minima.
+double order_parameter(Field<double> &phi) {
+    double OP = phi.sum();
+    return OP / lattice.volume();
 }
 
 // Construct some kind of update algorithm. Here we have a standard
 // Metropolis-Hastings algorithm that creates a proposal field for the
 // final multicanonical acceptance in the end.
-void multican_update(Field<double> &phi, Parity PAR) {
+void multican_update(hila::Muca &muca, Field<double> &phi, Parity PAR) {
     // Get a temporary field
     Field<double> new_phi = phi;
 
     // Get a field of random numbers
     Field<double> delta = 0;
-    onsites(PAR) hila::gaussian_random(delta[X]);
+    onsites (PAR)
+        hila::gaussian_random(delta[X]);
 
     // Add change to temporary field
     new_phi += 0.5 * delta;
@@ -51,7 +55,7 @@ void multican_update(Field<double> &phi, Parity PAR) {
     // Get the local differences in actions
     auto old_action = compute_local_action(phi);
     auto new_action = compute_local_action(new_phi);
-    onsites(PAR) {
+    onsites (PAR) {
         // compute log(exp(- delta S))
         double log_P = -(new_action[X] - old_action[X]);
         // Get a random uniform from [0,1] and reject based on that
@@ -65,16 +69,9 @@ void multican_update(Field<double> &phi, Parity PAR) {
     double OP_new = order_parameter(new_phi);
 
     // Use the multicanonical accept_reject for a final update decision
-    if (hila::muca::accept_reject(OP_old, OP_new)) {
+    if (muca.accept_reject(OP_old, OP_new)) {
         phi = new_phi;
     }
-}
-
-// Mean value of phi is a convenient order parameter, since its cheap
-// to compute and has different values in the different minima.
-double order_parameter(Field<double> &phi) {
-    double OP = phi.sum();
-    return OP / lattice.volume();
 }
 
 // The weight iteration consists of a simple loop
@@ -82,20 +79,21 @@ double order_parameter(Field<double> &phi) {
 // of the order parameter. The status informs us when the iteration
 // algorithm thinks its done and kills the loop. We save the weight data
 // for good measure.
-void iterate_weights(Field<double> &phi) {
+void iterate_weights(hila::Muca &muca, Field<double> &phi) {
     bool iterate_status = true;
     while (iterate_status) {
         // Perform a number of updates
         for (int i = 0; i < 25; i++) {
-            multican_update(phi, ODD);
-            multican_update(phi, EVEN);
+            multican_update(muca, phi, ODD);
+            multican_update(muca, phi, EVEN);
         }
         // compute and feed the order parameter of the configuration to the
         // iterator
         double OP = order_parameter(phi);
-        iterate_status = hila::muca::iterate_weights(OP);
+        iterate_status = muca.iterate_weights(muca, OP);
     }
-    hila::muca::write_weight_function(hila::muca::generate_outfile_name());
+    if (hila::myrank() == 0)
+        muca.write_weight_function(hila::generate_outfile_name("weight_func"));
 }
 
 // Here we combine the above functions into a full simulation that
@@ -109,44 +107,49 @@ int main(int argc, char *argv[]) {
     // hila::finishrun();
 
     lattice.setup({12, 12, 12});
-    hila::seed_random(0);
+    hila::seed_random(18067657112251);
 
-    hila::muca::initialise("muca_parameters");
+    hila::Muca muca = {0};
+    if (!muca.initialise("muca_parameters"))
+        hila::finishrun();
 
     // Initialise a field
     Field<double> phi = 0;
 
     // Open a file for measurements
-    bool muca = hila::cmdline.flag_present("-muca");
+    bool do_muca = hila::cmdline.flag_present("-muca");
     std::ofstream MFile;
-    if (muca) {
-        hila::out0 << "Running a multicanonical simulation.\n";
-        MFile.open("muca_measurements", std::ios_base::app);
-    } else {
-        hila::out0 << "Running a standard simulation.\n";
-        MFile.open("ca_measurements", std::ios_base::app);
+    if (hila::myrank() == 0) {
+        if (do_muca) {
+            hila::out0 << "Running a multicanonical simulation.\n";
+            MFile.open("muca_measurements", std::ios_base::app);
+        } else {
+            hila::out0 << "Running a standard simulation.\n";
+            MFile.open("ca_measurements", std::ios_base::app);
+        }
     }
 
-    if (muca)
-        iterate_weights(phi);
+    if (do_muca)
+        iterate_weights(muca, phi);
 
     const int N = 10000;
     // Get some measurements
     for (int i = 1; i <= N; i++) {
         double OP = order_parameter(phi);
-        char buffer[1024];
         // Remember that you need the weight of each measured configuration
         // for the post-processing!
-        sprintf(buffer, "%d\t%e\t%e\n", i, OP, hila::muca::weight(OP));
-        if (hila::myrank() == 0)
+        if (hila::myrank() == 0) {
+            char buffer[1024];
+            sprintf(buffer, "%d\t%e\t%e\n", i, OP, muca.weight_function(OP));
             MFile << std::string(buffer);
+        }
 
         if (i % (N / 100) == 0)
             hila::out0 << 100 * i / N << "% done\n";
         // Perform a set of updates
         for (int i = 0; i < 50; i++) {
-            multican_update(phi, ODD);
-            multican_update(phi, EVEN);
+            multican_update(muca, phi, ODD);
+            multican_update(muca, phi, EVEN);
         }
     }
 
