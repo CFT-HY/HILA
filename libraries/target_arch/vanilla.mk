8ed865e8d48528bbf3b733f9ae7b992024fec4f6
Kari Rummukainen <kari.rummukainen@helsinki.fi>
2026-03-09 20:48:42 +0200

    add -ffast-math to compiler options

--- libraries/target_arch/vanilla.mk
+++ libraries/target_arch/vanilla.mk
@@ -13,9 +13,9 @@ LD := mpic++
 
 # Define compilation flags
 ifndef DEBUG
-	CXXFLAGS := -O3 -x c++ --std=c++17 -fno-rtti -mavx2 -mfma
+	CXXFLAGS := -O3 -x c++ --std=c++17 -fno-rtti -mavx2 -mfma -ffast-math
 else
-	CXXFLAGS := -g -O3 -x c++ --std=c++17 -fno-rtti 
+	CXXFLAGS := -g -O3 -x c++ --std=c++17 -fno-rtti -mavx2 -mfma -ffast-math
 endif
 
 # -fsanitize=address -fno-omit-frame-pointer
